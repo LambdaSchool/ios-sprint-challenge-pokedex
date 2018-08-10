569040f5cd36d3b6b185946949d24ba161daa716
Conner Alegre <cga0@pm.me>
2018-08-10 09:47:05 -0600

    add base pokemon model

---
+++ ios-pokedex-rest/ios-pokedex-rest/Models/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  ios-pokedex-rest
+//
+//  Created by Conner on 8/10/18.
+//  Copyright © 2018 Conner. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable, Equatable {
+    var name: String
+    var id: String
+}
