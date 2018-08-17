ae69f2bec98f9088e4073b251ebd8c81fbabf95c
iyin raphael <iyinraphael@gmail.com>
2018-08-17 09:39:28 -0700

    added model controller

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,51 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Ngozi Ojukwu on 8/17/18.
+//  Copyright © 2018 iyin. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable, Equatable {
+    let name: String
+    let id: Int
+    let pokeAbilities: String?
+    let pokeTypes: String?
+    
+    let abilities:[Ability]?
+    let types: [pType]?
+    
+    init(name: String, id: Int, ability: String, type: String) {
+        self.name = name
+        self.id = id
+        self.pokeAbilities = ability
+        self.pokeTypes = type
+        self.abilities = nil
+        self.types = nil
+    }
+    
+    
+    struct Ability: Codable, Equatable{
+        let ability: trueAbility
+        struct trueAbility: Codable, Equatable{
+            let name:String
+        }
+        
+    }
+    
+    
+    struct pType: Codable, Equatable {
+        let type:trueType
+        struct trueType: Codable,Equatable{
+            let name: String
+        }
+    }
+    
+}
+
+struct Abilities: Codable {
+    
+}
+
