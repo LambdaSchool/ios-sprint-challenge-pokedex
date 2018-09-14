3717d6fcb9ddd2273e26e6829921e3a5f743b713
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-14 09:11:15 -0600

    Lay out views and add view controllers

---
+++ Pokedex/Pokedex/View/PokemonDetailViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokedex
+//
+//  Created by Dillon McElhinney on 9/14/18.
+//  Copyright © 2018 Dillon McElhinney. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func savePokemon(_ sender: Any) {
+        
+    }
+    
+}
