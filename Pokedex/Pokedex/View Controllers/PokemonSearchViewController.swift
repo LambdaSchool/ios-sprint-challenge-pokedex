c75282093465efb74fd41e1b895900c887590c6e
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-21 09:22:49 -0700

    subclassed controllers

---
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by Welinkton on 9/21/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
