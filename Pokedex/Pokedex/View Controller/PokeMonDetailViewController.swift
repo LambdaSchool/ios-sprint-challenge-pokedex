3b88b02fcab2653e0d4265c49da64c7bf2b5a81c
iyinraphael <iyinraphael@gmail.com>
2018-09-21 11:31:09 -0700

    table view controller

--- Pokedex/Pokedex/View Controller/PokeMonDetailViewController.swift
+++ Pokedex/Pokedex/View Controller/PokeMonDetailViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
-class PokeMonDetailViewController: UIViewController {
+class PokeMonDetailViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else {return}
+        pokeController?.fetchPokemon(name: searchTerm, completion: { (error) in
+            <#code#>
+        })
+    }
+    
     @IBAction func savePokemon(_ sender: Any) {
     }
+    
+    var pokemon: Pokemon?
+    var pokeController: PokemonController?
+    
     @IBOutlet weak var search: UISearchBar!
-    
     @IBOutlet weak var nameLabel: UILabel!
-    
     @IBOutlet weak var typesLabel: UILabel!
-    
     @IBOutlet weak var abilityLabel: UILabel!
-    
     @IBOutlet weak var idLabel: UILabel!
 }
