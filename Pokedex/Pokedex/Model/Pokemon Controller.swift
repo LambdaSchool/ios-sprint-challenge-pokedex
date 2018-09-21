496276e872093096e352e25f3508226240b46aba
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-21 09:12:34 -0700

    finished search for Pokemon search

--- Pokedex/Pokedex/Model/Pokemon Controller.swift
+++ Pokedex/Pokedex/Model/Pokemon Controller.swift
@@ -25,12 +25,42 @@ class PokemonController {
     // Delete Pokemon
     
     
-    //Search API ( URL, Session, dataTasks, errors, resume
+    //Search API ( URL, Session, JSON Decode, dataTasks, errors, resume
     func searchPokemon(name:String, completion: @escaping (Error?) -> Void){
         
         let requestURL = baseURL.appendingPathComponent("api").appendingPathComponent("v2").appendingPathComponent("pokemon").appendingPathComponent("name")
         
         var request = URLRequest(url: requestURL)
         request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                NSLog("Error grabbing pokemon info from server \(error)")
+                completion(NSError())
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("No data returned")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+            do{
+                let pokemonGrab = try jsonDecoder.decode(Pokemon.self, from: data)
+                print(pokemonGrab)
+                
+                self.pokemon = pokemonGrab
+                
+                completion(nil)
+            }catch {
+                NSLog("Error decoding pokemon from server \(error)")
+                completion(nil)
+                return
+            }
+        }.resume()
     }
 }
