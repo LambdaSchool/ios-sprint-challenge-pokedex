8736b324d40ef8bc85b9bceef9745f2974034ae5
Scott Bennett <stt.btt@gmail.com>
2018-09-21 11:11:45 -0400

    Initial cleanup

---
+++ Pokedex/Pokedex/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Scott Bennett on 9/21/18.
+//  Copyright © 2018 Scott Bennett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
