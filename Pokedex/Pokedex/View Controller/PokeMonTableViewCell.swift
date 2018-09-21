62f26923b970e17843ae36d7f2307013782afa5c
iyinraphael <iyinraphael@gmail.com>
2018-09-21 11:03:53 -0700

    cell details added

--- Pokedex/Pokedex/View Controller/PokeMonTableViewCell.swift
+++ Pokedex/Pokedex/View Controller/PokeMonTableViewCell.swift
@@ -9,6 +9,22 @@
 import UIKit
 
 class PokeMonTableViewCell: UITableViewCell {
+    
+    func updateView(){
+        if let pokemon = pokemon{
+            nameLabel.text = pokemon.name
+            guard let imageURL = URL(string: pokemon.sprites[0].spriteName) else {return}
+            guard let data = try? Data(contentsOf: imageURL) else {return}
+            pokeImageView.image = UIImage(data: data)
+        }
+    }
+    
+    var pokemonController: PokemonController?
+    var pokemon: Pokemon?{
+        didSet{
+            updateView()
+        }
+    }
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var pokeImageView: UIImageView!
     
