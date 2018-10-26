04b913fb2c09932112bf4115715d2eaeab6a81fc
OmegaWulf <nikcxz@live.com>
2018-10-26 11:07:16 -0400

    Storyboard setup

---
+++ Pokedex/Pokedex/PokedexTVC.swift
@@ -0,0 +1,14 @@
+//
+//  PokedexTVC.swift
+//  Pokedex
+//
+//  Created by Nikita Thomas on 10/26/18.
+//  Copyright © 2018 Nikita Thomas. All rights reserved.
+//
+
+import UIKit
+
+class PokedexTVC: UITableViewController {
+    
+    
+}
---
+++ Pokedex/Pokedex/SearchTVC.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Pokedex
+//
+//  Created by Nikita Thomas on 10/26/18.
+//  Copyright © 2018 Nikita Thomas. All rights reserved.
+//
+
+import UIKit
+
+class SearchTVC: UITableViewController {
+    
+    
+}
