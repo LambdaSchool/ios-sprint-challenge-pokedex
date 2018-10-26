562f93e7632cba0c58f2d572d2fc0377723f282d
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-26 10:44:15 -0700

    fixed search bar... chose wrong one. smh

--- Pokedex/Pokedex/SearchViewController.swift
+++ Pokedex/Pokedex/SearchViewController.swift
@@ -14,6 +14,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonSearchBar.delegate = self
     }
     
 }
