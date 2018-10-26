f715cd887e3ad8509d84ddb21b9605c81d31e18e
Jerrick Warren <Jerrick.Warren@gmail.com>
2018-10-26 10:03:07 -0700

    added searchViewController. running into issues

---
+++ Pokedex/Pokedex/SearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchViewController.swift
+//  Pokedex
+//
+//  Created by Jerrick Warren on 10/26/18.
+//  Copyright © 2018 Jerrick Warren. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var pokemonSearchBar: UISearchBar!
+    
+}
--- Pokedex/Pokedex/TableViewController.swift
+++ Pokedex/Pokedex/TableViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    
+    let reuseIdentifier = "pokemoncell"
     
     
 }
