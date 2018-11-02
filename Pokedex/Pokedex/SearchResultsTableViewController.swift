2fc4c9fca021db33aeb90be201c10feb8bb9bbb3
Sean Doyle <sdoylelambda@gmail.com>
2018-11-02 13:59:16 -0400

    final submission

--- Pokedex/Pokedex/SearchResultsTableViewController.swift
+++ Pokedex/Pokedex/SearchResultsTableViewController.swift
@@ -7,7 +7,7 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
 
 //       searchBar.delegate = self
     }
-
+    
     // MARK: - Table view data source
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let search = searchBar.text, search.count > 0 else { return }
@@ -40,6 +40,48 @@ class SearchResultsTableViewController: UITableViewController, UISearchBarDelega
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+   
+//    let addSegueIdentifier = "searchSegue"
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        guard let searchResultsController = segue.destination as? ListTableViewController else { fatalError("Segue destination failed")
+//
+//        }
+//
+//        guard let identifier = segue.identifier
+//            else { fatalError("No segue identifier provided") }
+//
+//        switch identifier {
+//        case addSegueIdentifier:
+//            ListTableViewController.indexPath = nil
+//
+//        case addSegueIdentifier:
+//            guard let indexPath = tableView.indexPathForSelectedRow
+//                else { fatalError("Unable to retrieve selected index path") }
+//            ListTableViewController.indexPath = indexPath
+//
+//        default:
+//            fatalError("Unknown segue identifier: \(identifier)")
+//        }
+//    }
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//        tableView.reloadData()
+//    }
+    
 
+    
+    // swipe to delete
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard editingStyle == .delete else { return }
+        
+        // Update model then refresh view
+        
+        if (editingStyle == .delete){
+            SearchResultController.shared.searchResults.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
 }
-
+}
+}
