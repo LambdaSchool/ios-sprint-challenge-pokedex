3d4e3b36b3d98f4e366842a75a41da8e6467b9cf
Frulwinn <frulwinn@gmail.com>
2018-12-07 09:50:58 -0800

    created Poke to fetch data from api

--- Pokedex/Pokedex/Model.swift
+++ Pokedex/Pokedex/Model.swift
@@ -1,5 +1,30 @@
 import Foundation
 
 class Model {
+    static let shared = Model()
+    private init() {}
     
+    typealias UpdateHandler = () -> Void
+    var updateHandler: UpdateHandler? = nil
+    
+    var characters: [Character] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateHandler?()
+            }
+        }
+    }
+    func numberOfCharacters() -> Int {
+        return characters.count
+    }
+    func search(for string: String) {
+        Poke.searchForCharacters(with: string) { people, error in
+            if let error = error {
+                NSLog("Error fetching people: \(error)")
+                return
+            }
+            
+            self.characters = characters ?? []
+        }
+    }
 }
