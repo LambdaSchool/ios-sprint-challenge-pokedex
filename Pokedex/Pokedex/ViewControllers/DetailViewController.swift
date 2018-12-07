5f56dfeb578ab2d5180079945a7b7c4705eca35a
Ben Hakes <bhhakes@gmail.com>
2018-12-07 10:31:40 -0600

    add detailVC code:

--- Pokedex/Pokedex/ViewControllers/DetailViewController.swift
+++ Pokedex/Pokedex/ViewControllers/DetailViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+    var pokemon: Pokemon?
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let pokemon = pokemon else { return }
+        nameLabel.text = pokemon.name
+        idLabel.text = pokemon.id
+        typesLabel.text = pokemon.id
+        abilitiesLabel.text = pokemon.abilities
     }
-    */
 
 }
