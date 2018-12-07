4fd967738627a3379b606851cea6b068eecc0194
Ben Hakes <bhhakes@gmail.com>
2018-12-07 10:41:07 -0600

    add Pokemon class

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class Pokemon: Codable, FirebaseItem {
+    
+    var name: String
+    var id: String
+    var types: String
+    var abilities: String
+    var recordIdentifier: String = ""
+    
+    init(name: String, id: String, types: String?, abilities: String?) {
+        let types = types ?? ""
+        let abilities = abilities ?? ""
+        (self.name, self.id,self.types, self.abilities ) = (name, id, types, abilities)
+    }
+}
