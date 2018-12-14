b43b6422d0711008d4c6e17cd66413e31603b394
Frulwinn <frulwinn@gmail.com>
2018-12-14 10:03:37 -0800

    added tableview funcs and prepare for segue func

--- SprintThree/SprintThree/DetailViewController.swift
+++ SprintThree/SprintThree/DetailViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    var pokemon: Pokemon?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- SprintThree/SprintThree/TableViewController.swift
+++ SprintThree/SprintThree/TableViewController.swift
@@ -2,35 +2,34 @@ import UIKit
 
 class TableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-      
+    @IBAction func search(_ sender: Any) {
     }
 
     // MARK: - Table view data source
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return Model.shared.numberOfPokemons()
     }
 
-
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath)
+        let pokemon = Model.shared.pokemon(at: indexPath)
+        
+        cell.textLabel?.text = pokemon.name
         return cell
     }
 
-    // Override to support editing the table view.
-//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-//        guard let editingStyle == .delete else {return}
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
     }
 
     // MARK: - Navigation
-
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       
+        guard let destination = segue.destination as? DetailViewController,
+            let indexPath = tableView.indexPathForSelectedRow else {return}
+        
+        let pokemon = Model.shared.pokemon(at: indexPath)
+        destination.pokemon = pokemon
         
     }
 }
