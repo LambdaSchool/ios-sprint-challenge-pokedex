cbec62d3249461143ea054b1c7260d8808562de7
Sean Doyle <sdoylelambda@gmail.com>
2018-12-14 11:58:07 -0500

    Model added

--- 12.14 Pokedex Sprint 3/12.14 Pokedex Sprint 3/Pokemon.swift
+++ 12.14 Pokedex Sprint 3/12.14 Pokedex Sprint 3/Pokemon.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-class Pokemon: Codable {
+struct Pokemon: Codable {
+    let pokemonName: String
+    let pokemonID: String
+    let pokemonType: String
+    let pokemonAbilities: String
+    let pokemonImage: imageURLS
     
+}
+
+struct imageURLS: Codable {
+    let name: String
+    let url: String
+    let ability: String
+}
+
+struct SearchResults: Codable {
+    let results: [Pokemon]
 }
