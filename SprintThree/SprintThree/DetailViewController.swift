b039aec94441d151e504d45971f0fe1062019fd7
Frulwinn <frulwinn@gmail.com>
2018-12-14 10:10:10 -0800

    made searchbar delegate

--- SprintThree/SprintThree/DetailViewController.swift
+++ SprintThree/SprintThree/DetailViewController.swift
@@ -16,7 +16,7 @@ class DetailViewController: UIViewController, UISearchBarDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBar.delegate = self
     }
 
 
