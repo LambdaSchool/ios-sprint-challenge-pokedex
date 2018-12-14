c79db874a27cde5f7518a3279f544627d9b7f7be
Frulwinn <frulwinn@gmail.com>
2018-12-14 10:20:40 -0800

    add save button action code

--- SprintThree/SprintThree/DetailViewController.swift
+++ SprintThree/SprintThree/DetailViewController.swift
@@ -11,14 +11,36 @@ class DetailViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var abilitiesLabel: UILabel!
     @IBOutlet weak var pokemonView: UIImageView!
     @IBAction func save(_ sender: Any) {
+        guard let pokemon = pokemon else {return}
+        guard let name = nameLabel.text, !name.isEmpty else {return}
+        Model.shared.addPokemon(pokemon: pokemon)
+        
+        navigationController?.popViewController(animated: true)
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchBar.delegate = self
+        updateViews()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else {return}
+        
+        searchBar.text = ""
+        
+        Model.shared.search(for: searchTerm) { (error) in
+            if error == nil {
+                DispatchQueue.main.async {
+                    updateViews()
+                }
+            }
+        }
     }
 
-
+    func updateViews() {
+        
+    }
 }
 
