7437eef5629c2df6cfb571df2464004e8087e0c6
Frulwinn <frulwinn@gmail.com>
2018-12-14 10:09:02 -0800

    hooked up outlets for DetailViewController

--- SprintThree/SprintThree/DetailViewController.swift
+++ SprintThree/SprintThree/DetailViewController.swift
@@ -1,8 +1,18 @@
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UISearchBarDelegate {
 
     var pokemon: Pokemon?
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var pokemonView: UIImageView!
+    @IBAction func save(_ sender: Any) {
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
