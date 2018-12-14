583e27f6ee183c5c4c9abb4bee3bb3ff4a3e69fb
Vijay Das <vijaytdas@gmail.com>
2018-12-14 12:49:18 -0600

    Work in progress: Pokemon record, PokemonModel with API logic, and PokemonTableViewController

--- PokeDex/PokeDex/Models/PokemonModel.swift
+++ PokeDex/PokeDex/Models/PokemonModel.swift
@@ -12,7 +12,28 @@ class PokemonModel {
     static let shared = PokemonModel()
     private init () {}
     
-//    private let baseURL = URL(string: )
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")
+    
+    private var pokemons: [Pokemon] = []
+    
+    var numberOfPokemon: Int {
+        return pokemons.count
+    }
+    
+    func pokemon(at indexPath: IndexPath) -> Pokemon {
+        return pokemons[indexPath.row]
+    }
+    
+    
+    func search(for searchTerm: String, completion: @escaping (Error?) -> Void ) {
+        var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        
+        // need to figure out how the API accepts search
+        
+    }
+    
+    // dataTask here
+    
     
     
     
