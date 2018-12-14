44533357eaa56045c4d1484b74be462524911ff2
Frulwinn <frulwinn@gmail.com>
2018-12-14 08:36:08 -0800

    added singleton to model, baseURL, and variable

--- SprintThree/SprintThree/Model.swift
+++ SprintThree/SprintThree/Model.swift
@@ -2,4 +2,13 @@ import Foundation
 
 class Model {
     
+    //singleton
+    static let shared = Model()
+    private init () {}
+    
+    //baseURL for api
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/")!
+    
+    //var
+    var pokemons: [Pokemon] = []
 }
