4835764a193a8f0cc1d60b9467e9cced56dd9d7c
Sean Doyle <sdoylelambda@gmail.com>
2018-12-14 11:22:25 -0500

    Skeleton files added, outlets connected.

---
+++ 12.14 Pokedex Sprint 3/12.14 Pokedex Sprint 3/PokemonViewController.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+class PokemonViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameLabelSame: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitesLabel: UILabel!
+    @IBOutlet weak var searchBarOutlet: UISearchBar!
+    
+    @IBAction func savePokemon(_ sender: Any) {
+        // save search bar text to model
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
