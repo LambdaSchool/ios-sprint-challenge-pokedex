5bed65b38e502c29f5cbac48e561494f0ca32fec
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-19 21:59:49 -0800

    Simplify the API and dedicate saving selected Pokemon to the model in a seperate file
    - Reviewing the training kit has been helpful
    - adjust the search to have the characters in the searchTerm set to be lowercased()

--- PokeAPI Sprint 3/Models/PokeAPI.swift
+++ PokeAPI Sprint 3/Models/PokeAPI.swift
@@ -2,21 +2,21 @@ import UIKit
 
 class PokemonSearchResultsController {
     
-    static let shared = PokemonSearchResultsController()
+    // static let shared = PokemonSearchResultsController()
     
     let baseURL = "https://pokeapi.co/api/v2/pokemon/"
     
     // search results array, data source for the table view
-    var searchResults: [Pokemon] = []
+    // var pokemonSearchResults: [Pokemon] = []
     
-    func performSearch(searchTerm: String, completion: @escaping ([Pokemon]?, NSError?) -> Void) {
+    func performSearch(searchTerm: String, completion: @escaping (NSError?) -> Void) {
         
-        let requestURL = URL(string: baseURL + searchTerm)
+        let requestURL = URL(string: baseURL + searchTerm.lowercased())
         
         guard let searchRequestURL = requestURL
             else {
                 NSLog("Could not make URL from components.")
-                completion(nil, NSError())
+                completion(NSError())
                 return
         }
         
@@ -28,27 +28,29 @@ class PokemonSearchResultsController {
             // Check for errors. If there is an error, call completion with the error.
             if let error = error {
                 NSLog("Error fetching data: \(error)")
-                completion(nil, NSError())
+                completion(NSError())
                 return
             }
             
             // Check for errors. If there is an error, call completion with the error.
             guard let data = data else {
                 NSLog("No data returned from data task.")
-                completion(nil, NSError())
+                completion(NSError())
                 return
             }
             
             do {
-                let searchResults = try JSONDecoder().decode(PokemonSearchResults.self, from: data)
-               self.searchResults = searchResults.results
-                completion(self.searchResults, nil)
+                // Try to obtain a Pokemon from the API
+                let pokemonSearchResults = try JSONDecoder().decode(Pokemon.self, from: data)
+                // Store it in our Model
+                PokedexModel.shared.selectedPokemon = pokemonSearchResults
+                completion(nil)
    
                 return
                 
             } catch {
                 NSLog("Unable to decode data: \(error)")
-                completion(nil, NSError())
+                completion(NSError())
                 return
             }
         }
