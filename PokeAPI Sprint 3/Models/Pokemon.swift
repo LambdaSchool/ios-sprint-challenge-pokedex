f3c519b02471b1e20febd82388801364517413c0
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-19 19:15:06 -0800

    nest structs to make life easier
    special thanks to @iyinraphael
    - renamed Other to Name, as that is what is being pointed to.
    - commented out the bottom to reduce reliance on it and any confusion that has occurred as a result.

--- PokeAPI Sprint 3/Models/Pokemon.swift
+++ PokeAPI Sprint 3/Models/Pokemon.swift
@@ -2,31 +2,31 @@ import Foundation
 struct Pokemon: Codable {
     let name: String
     let id: Int
-    let abilities: [Ability]
+    let abilities: [Abilities]
     let types: [Types]
-    let sprites: Sprites
-}
+    let sprites: Sprites?
 
-struct Types: Codable {
-    let type: Other
-}
-
-struct Ability: Codable {
-    let ability: Other
-}
-
-struct Other: Codable {
-    let name: String
-}
-
-struct Sprites: Codable {
-    let frontDefault: String
+    struct Types: Codable {
+        let type: Name
+    }
     
-    enum CodingKeys: String, CodingKey {
-        case frontDefault = "front_default"
+    struct Abilities: Codable {
+        let ability: Name
+    }
+    
+    struct Name: Codable {
+        let name: String
+    }
+    
+    struct Sprites: Codable {
+        let frontDefault: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case frontDefault = "front_default"
+        }
     }
 }
 
-struct PokemonSearchResults: Codable {
-    let results: [Pokemon]
-}
+//struct PokemonSearchResults: Codable {
+//    let results: [Pokemon]
+//}
