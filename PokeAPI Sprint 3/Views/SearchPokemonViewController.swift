56a9398ddd771436617b0d0928e57ec7047d0bab
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-12-19 23:37:09 -0800

    finally shows that a pokemon can be obtained from the API

--- PokeAPI Sprint 3/Views/SearchPokemonViewController.swift
+++ PokeAPI Sprint 3/Views/SearchPokemonViewController.swift
@@ -1,70 +1,82 @@
 import UIKit
 
 class SearchPokemonViewController: UIViewController, UISearchBarDelegate {
-
-    static let reuseIdentifer = "ResultCell"
     
+    // Pokemon Name
     @IBOutlet weak var pokemonNameLabel: UILabel!
-    
+    // Pokemon ID
     @IBOutlet weak var pokemonIDLabel: UILabel!
-    
+    // Pokemon Type(s)
     @IBOutlet weak var pokemonTypesLabel: UILabel!
-    
+    // Pokemon Abilities
     @IBOutlet weak var pokemonAbilitiesLabel: UILabel!
-    
+    // Pokemon Sprite/Image
     @IBOutlet weak var pokemonSpriteImageView: UIImageView!
-    
+    // Save Pokemon that is in the results.
     @IBAction func savePokemon(_ sender: Any) {
-        pokemonSearchResultsController.pokemonSearchResults.append(pokemonSearchResult!)
+        // Connect to our model and call on the save Pokemon function to save it to our dedicated array.
+        PokedexModel.shared.savePokemon()
         
         // Pop back into previous view after saving pokemon
         navigationController?.popViewController(animated: true)
     }
     
+    // Relocate this to the model if necessary.
     var pokemonSearchResult: Pokemon?
     
-    // Access our model and save having to write a few characters...
+    // In case we need to connect to our API...
     let pokemonSearchResultsController = PokemonSearchResultsController()
     
+    // SearchBar Outlet
     @IBOutlet weak var searchBar: UISearchBar!
     
-    // Fix later
+    // Set it to something (elsewhere) or remove completely
     @IBOutlet weak var searchResultsVCTitle: UINavigationItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Set up searchbar delegate in viewDidLoad
         searchBar.delegate = self
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Something we added from a different project.
         searchBar.resignFirstResponder()
+        
+        // Creates a constant set to the text in the searchbar and checks if it's empty.
         guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
         
+        // Sets the searchbar text to empty after it has been used
         searchBar.text = ""
+        // Creates a cool gray placeholder to show what the user typed in
+        // which is more neat than useful in this case, given our data
         searchBar.placeholder = searchTerm
         
         func updateSearchResults() {
             
-            if let pokemonSearchResult = pokemonSearchResult {
+            // if let pokemonSearchResult = pokemonSearchResult {
                 
-                guard let urlString = pokemonSearchResult.sprites?.frontDefault else { return }
+                guard let presentedPokemon = PokedexModel.shared.selectedPokemon else { fatalError("Could not obtain Pokemon.")}
                 
-                guard let imageURL = URL(string: urlString), let imageData = try? Data(contentsOf: imageURL) else { fatalError("Couldn't obtain image.")}
-                self.pokemonNameLabel.text = pokemonSearchResult.name
-                self.pokemonIDLabel.text = "ID: \(pokemonSearchResult.id)"
+                self.pokemonNameLabel.text = presentedPokemon.name
+                self.pokemonIDLabel.text = "ID: \(presentedPokemon.id)"
                 
                 self.pokemonTypesLabel.text = "Types: "
                 
                 self.pokemonAbilitiesLabel.text = "Abilities: "
                 
-               self.pokemonSpriteImageView?.image = UIImage(data: imageData)
-            }
-        }
+                // Unwrap the URL if a valid, non-empty sprite is returned
+                guard let urlString = presentedPokemon.sprites?.frontDefault else { return }
+                // Set the link to the image
+                guard let imageURL = URL(string: urlString), let imageData = try? Data(contentsOf: imageURL) else { fatalError("Couldn't obtain image.")}
+                // Set the ImageView to the imageData in the form of a usable UIImage
+                self.pokemonSpriteImageView?.image = UIImage(data: imageData)
+        //    }
+         }
         
         pokemonSearchResultsController.performSearch(searchTerm: searchTerm) { _ in
             DispatchQueue.main.async {
-               // self.pokemonSearchResult = PokemonSearchResultsController.shared.pokemonSearchResults[IndexPath]
+                // self.pokemonSearchResult = PokemonSearchResultsController.shared.pokemonSearchResults[IndexPath]
                 updateSearchResults()
             }
         }
