91279a60bf80a66d746188e9ebbc17b5a3956d3a
Sean Doyle <sdoylelambda@gmail.com>
2019-01-03 16:25:28 -0500

    Initial commit.

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Lambda_School_Loaner_18 on 1/3/19.
+//  Copyright © 2019 Lambda_School_Loaner_18. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
