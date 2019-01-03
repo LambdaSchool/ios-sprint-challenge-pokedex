bf3dc6baf706ff8d4e19efbbed6c71f3c8e027ba
Sean Doyle <sdoylelambda@gmail.com>
2019-01-03 17:23:50 -0500

    Basic Model Completed.

--- Pokedex/Pokedex/Data/Model.swift
+++ Pokedex/Pokedex/Data/Model.swift
@@ -2,4 +2,24 @@ import Foundation
 
 class Model {
     
+    static let shared = Model()
+    private init() {}
+    var pokemon: Pokemon?
+    var savedPokemon: [Pokemon] = []
+    
+    func numberOfpokemon() -> Int {
+        return savedPokemon.count
+    }
+    
+    func addPokemon(pokemon: Pokemon) {
+        savedPokemon.append(pokemon)
+    }
+    
+    func removePokemon(at indexPath: IndexPath) {
+        savedPokemon.remove(at: indexPath.row)
+    }
+    
+    func savePokemon(at indexPath: IndexPath) -> Pokemon {
+        return savedPokemon[indexPath.row]
+    }
 }
