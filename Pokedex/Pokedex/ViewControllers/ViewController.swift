6d6d20c4c3c241f9d87393cd0e9321e24b06478b
Sean Doyle <sdoylelambda@gmail.com>
2019-01-03 19:11:49 -0500

    Saved PokemonVC completed.

--- Pokedex/Pokedex/ViewControllers/ViewController.swift
+++ Pokedex/Pokedex/ViewControllers/ViewController.swift
@@ -7,9 +7,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var typesLabel: UILabel!
     @IBOutlet weak var abilitiesLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var viewController = ViewController()
+    
+    var pokemon: Pokemon?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let pokemon = pokemon else { return }
+        
+        var typesArray: [String] = []
+        for each in pokemon.types {
+            typesArray.append(each.type.name.capitalized)
+        }
+        
+        var abilitiesArray: [String] = []
+        for each in pokemon.abilities {
+            abilitiesArray.append(each.ability.name.capitalized)
+        }
+        
+        self.navigationItem.title = pokemon.name.capitalized
+        nameLabel.text = pokemon.name.capitalized
+        IDLabel.text = "ID: \(pokemon.id)"
+        typesLabel.text = "Types: \(typesArray)"
+        abilitiesLabel.text = "Abilites: \(abilitiesArray)"
+        
+        guard let url = URL(string: pokemon.sprites.frontDefault),
+        let imageData = try? Data(contentsOf: url) else { return }
+        
+        pokemonImage.image = UIImage(data: imageData)
+        
+        
     }
 
 
