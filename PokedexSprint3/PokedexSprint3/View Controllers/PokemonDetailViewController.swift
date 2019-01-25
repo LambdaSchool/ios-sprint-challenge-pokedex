781f6c4a59d95514a1dff3a034050e8c938639db
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-25 10:52:21 -0800

    update remaining view controllers to obtain MVP

--- PokedexSprint3/PokedexSprint3/View Controllers/PokemonDetailViewController.swift
+++ PokedexSprint3/PokedexSprint3/View Controllers/PokemonDetailViewController.swift
@@ -12,13 +12,41 @@ class PokemonDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDetailView()
     }
     
+    @IBOutlet weak var pokemonTitleName: UINavigationItem!
     @IBOutlet weak var pokemonName: UILabel!
     @IBOutlet weak var pokemonID: UILabel!
     @IBOutlet weak var pokemonTypes: UILabel!
     @IBOutlet weak var pokemonAbilities: UILabel!
     @IBOutlet weak var pokemonSprite: UIImageView!
-    @IBOutlet weak var pokemonTitleName: UINavigationItem!
+    
+    
+    func updateDetailView() {
+
+        guard let presentedPokemon = self.savedPokemon else { fatalError("Could not obtain Pokemon.")}
+        
+        pokemonTitleName.title = presentedPokemon.name
+        
+        pokemonName.text = presentedPokemon.name
+        
+        pokemonID.text = "ID: \(presentedPokemon.id)"
+        
+        pokemonTypes.text = "Types: \(presentedPokemon.types.compactMap({$0.type.name}).joined(separator: ", "))"
+        
+        pokemonAbilities.text = "Abilities: \(presentedPokemon.abilities.compactMap({$0.ability.name}).joined(separator: ", "))"
+        
+        guard let urlString = presentedPokemon.sprites?.frontDefault else { return }
+        
+        guard let imageURL = URL(string: urlString), let imageData = try? Data(contentsOf: imageURL) else { fatalError("Couldn't obtain image.")}
+        
+        self.pokemonSprite?.image = UIImage(data: imageData)
+        
+    }
+    
+    // MARK: Properties
+    
+    var savedPokemon: Pokemon?
     
 }
