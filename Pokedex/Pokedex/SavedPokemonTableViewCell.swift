9d40065ab45eb1ad3ff7cc5368286486aaddfa87
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-25 12:33:07 -0500

    create class to attach cell to and make connections to outlets

---
+++ Pokedex/Pokedex/SavedPokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SavedPokemonTableViewCell.swift
+//  Pokedex
+//
+//  Created by Julian A. Fordyce on 1/25/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class SavedPokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
