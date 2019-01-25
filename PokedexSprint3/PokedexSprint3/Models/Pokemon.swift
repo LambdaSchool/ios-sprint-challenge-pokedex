9482d6a5030a61412f6ec1a20d2d8b17e9aa4cb4
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-25 10:03:50 -0800

    set up struct to communicate with Pokemon API

--- PokedexSprint3/PokedexSprint3/Models/Pokemon.swift
+++ PokedexSprint3/PokedexSprint3/Models/Pokemon.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+struct Pokemon: Codable {
+    
+    let name: String
+    let id: Int
+    let abilities: [Abilities]
+    let types: [Types]
+    let sprites: Sprites?
+    
+    struct Types: Codable {
+        let type: Name
+    }
+    
+    struct Abilities: Codable {
+        let ability: Name
+    }
+    
+    struct Name: Codable {
+        let name: String
+    }
+    
+    struct Sprites: Codable {
+        let frontDefault: String?
+        
+        enum CodingKeys: String, CodingKey {
+            case frontDefault = "front_default"
+        }
+    }
+}
