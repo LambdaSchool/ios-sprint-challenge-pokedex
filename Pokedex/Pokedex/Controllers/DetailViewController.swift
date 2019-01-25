c1c63d51ac6a401531f54f06a3555237ad940206
Frulwinn <frulwinn@gmail.com>
2019-01-25 10:17:37 -0800

    added updateViews on DetailViewController

--- Pokedex/Pokedex/Controllers/DetailViewController.swift
+++ Pokedex/Pokedex/Controllers/DetailViewController.swift
@@ -32,7 +32,24 @@ class DetailViewController: UIViewController, UISearchBarDelegate {
     }
     
     func updateViews() {
+        guard let pokemon = pokemon else { return }
         
+        DispatchQueue.main.async {
+            self.navigationItem.title = pokemon.name
+            self.nameLabel.text = pokemon.name
+            self.idLabel.text = "ID: \(pokemon.id)"
+            
+            let types: [String] = pokemon.types.map { $0.type.name }
+            self.typesLabel.text = "Types: \(types.joined(separator: ", "))"
+            
+            let abilities: [String] = pokemon.abilities.map { $0.ability.name }
+            self.abilitiesLabel.text = "Abilities: \(abilities.joined(separator: ", "))"
+            
+            guard let url = URL(string: pokemon.sprites.frontDefault),
+                let imageData = try? Data(contentsOf: url) else { return }
+            
+            self.pokemonView.image = UIImage(data: imageData)
+        }
     }
     
 }
