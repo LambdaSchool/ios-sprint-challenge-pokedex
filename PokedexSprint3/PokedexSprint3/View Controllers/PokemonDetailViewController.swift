c512f832bdde90ddfc8cead578c9c030beddc1b4
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-25 10:25:37 -0800

    make connections to the main storyboard

--- PokedexSprint3/PokedexSprint3/View Controllers/PokemonDetailViewController.swift
+++ PokedexSprint3/PokedexSprint3/View Controllers/PokemonDetailViewController.swift
@@ -12,19 +12,13 @@ class PokemonDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonID: UILabel!
+    @IBOutlet weak var pokemonTypes: UILabel!
+    @IBOutlet weak var pokemonAbilities: UILabel!
+    @IBOutlet weak var pokemonSprite: UIImageView!
+    @IBOutlet weak var pokemonTitleName: UINavigationItem!
+    
 }
--- PokedexSprint3/PokedexSprint3/View Controllers/PokemonSearchViewController.swift
+++ PokedexSprint3/PokedexSprint3/View Controllers/PokemonSearchViewController.swift
@@ -12,19 +12,20 @@ class PokemonSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var pokemonSearchBar: UISearchBar!
+    
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonID: UILabel!
+    @IBOutlet weak var pokemonTypes: UILabel!
+    @IBOutlet weak var pokemonAbilities: UILabel!
+    @IBOutlet weak var pokemonSprite: UIImageView!
+    
+    @IBAction func savePokemon(_ sender: Any) {
+        
     }
-    */
+    
+    
 
 }
