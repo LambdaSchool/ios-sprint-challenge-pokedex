486ee973cc4bd5d071fd718229dfff6786bc5c52
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2019-01-25 10:36:57 -0800

    update Pokemon Search VC
    - added a way to change the title name as well

--- PokedexSprint3/PokedexSprint3/View Controllers/PokemonSearchViewController.swift
+++ PokedexSprint3/PokedexSprint3/View Controllers/PokemonSearchViewController.swift
@@ -8,14 +8,15 @@
 
 import UIKit
 
-class PokemonSearchViewController: UIViewController {
+class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        pokemonSearchBar.delegate = self
     }
     
+    @IBOutlet weak var pokemonTitleName: UINavigationItem!
     @IBOutlet weak var pokemonSearchBar: UISearchBar!
-    
     @IBOutlet weak var pokemonName: UILabel!
     @IBOutlet weak var pokemonID: UILabel!
     @IBOutlet weak var pokemonTypes: UILabel!
@@ -23,9 +24,43 @@ class PokemonSearchViewController: UIViewController {
     @IBOutlet weak var pokemonSprite: UIImageView!
     
     @IBAction func savePokemon(_ sender: Any) {
-        
+        PokedexModel.shared.savePokemon()
+        navigationController?.popViewController(animated: true)
     }
     
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        searchBar.resignFirstResponder()
+        guard let searchTerm = searchBar.text, !searchTerm.isEmpty else { return }
+        searchBar.text = ""
+        searchBar.placeholder = searchTerm
+        
+        func updateSearchResults() {
+            
+            guard let presentedPokemon = PokedexModel.shared.selectedPokemon else { fatalError("Could not obtain Pokemon.")}
+            
+            pokemonTitleName.title = presentedPokemon.name
+            pokemonName.text = presentedPokemon.name
+            pokemonID.text = "ID: \(presentedPokemon.id)"
+            pokemonTypes.text = "Types: \(presentedPokemon.types.compactMap({$0.type.name}).joined(separator: ", "))"
+            pokemonAbilities.text = "Abilities: \(presentedPokemon.abilities.compactMap({$0.ability.name}).joined(separator: ", "))"
+            
+            guard let urlString = presentedPokemon.sprites?.frontDefault else { return }
+            
+            guard let imageURL = URL(string: urlString), let imageData = try? Data(contentsOf: imageURL) else { fatalError("Couldn't obtain image.")}
+            pokemonSprite?.image = UIImage(data: imageData)
+            
+        }
+        
+        pokemonSearchResultsController.performSearch(searchTerm: searchTerm) { _ in
+            DispatchQueue.main.async {
+                updateSearchResults()
+            }
+        }
+    }
     
+    // MARK: Properties
+    
+    let pokemonSearchResultsController = PokemonSearchResultsController()
 
 }
