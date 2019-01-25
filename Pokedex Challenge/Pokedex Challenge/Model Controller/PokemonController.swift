ce14b9f7223ce9da131e4e00dd7960ca51e892ae
Michael R.F <mrflowers22@uwalumni.com>
2019-01-25 11:55:55 -0500

    structure the model.

---
+++ Pokedex Challenge/Pokedex Challenge/Model Controller/PokemonController.swift
@@ -0,0 +1,15 @@
+//
+//  PokemonController.swift
+//  Pokedex Challenge
+//
+//  Created by Michael Flowers on 1/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+class PokemonController {
+    
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")! //using the "!" because if this doesn't work then I want the app to crash so I can handle the error
+    
+}
---
+++ Pokedex Challenge/Pokedex Challenge/Model/Pokemon.swift
@@ -0,0 +1,53 @@
+//
+//  Pokemon.swift
+//  Pokedex Challenge
+//
+//  Created by Michael Flowers on 1/25/19.
+//  Copyright © 2019 Michael Flowers. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable, Equatable {
+   
+    //REMEMBER EVERY OPEN BRACKET ON THE JSON means that we have to create a new struct.
+    let name: String
+    let id: Int
+    let abilities: [AbilityArray]
+    let types: [TypeArray]
+    let sprites: SpriteDictionary
+    
+    static func == (lhs: Pokemon, rhs: Pokemon) -> Bool {
+        return lhs.name == rhs.name && lhs.id == rhs.id && lhs.abilities == rhs.abilities && lhs.sprites == rhs.sprites && lhs.types == rhs.types
+    }
+    
+}
+
+struct AbilityArray: Codable, Equatable {
+    let ability: [AbilityDictionary]
+    
+    static func == (lhs: AbilityArray, rhs: AbilityArray) -> Bool {
+        return lhs.ability == rhs.ability
+    }
+}
+
+struct AbilityDictionary: Codable, Equatable {
+    let name: String
+    
+}
+
+struct TypeArray: Codable, Equatable {
+    let type:TypeDictionary
+}
+
+struct TypeDictionary: Codable, Equatable {
+    let name: String
+}
+
+struct SpriteDictionary: Codable, Equatable {
+    let imageURL: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case imageURL = "front_default"
+    }
+}
