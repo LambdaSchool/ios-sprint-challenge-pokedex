218f71c4a3dd7166d9a90efab5f857a8444628b5
Nelson Gonzalez <nelglezfl@gmail.com>
2019-01-24 23:40:33 -0500

    Fixed UI bugs and deletes scene to only have two.

--- PokedexApp/PokedexApp/View Controllers/PokeListTableViewController.swift
+++ PokedexApp/PokedexApp/View Controllers/PokeListTableViewController.swift
@@ -9,24 +9,35 @@
 import UIKit
 
 class PokeListTableViewController: UITableViewController {
+    
+ //   var pokemonController = PokemonController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 0
+        return PokemonController.shared.pokemons.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "pokeCell", for: indexPath)
 
+        let pokemons = PokemonController.shared.pokemons[indexPath.row]
         
+        cell.textLabel?.text = pokemons.name
+        guard let imageUrl = URL(string: pokemons.sprites.frontDefault), let imageData = try? Data(contentsOf: imageUrl) else {return cell}
+        cell.imageView?.image = UIImage(data: imageData)
 
         return cell
     }
@@ -34,7 +45,11 @@ class PokeListTableViewController: UITableViewController {
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
+            let pokemon = PokemonController.shared.pokemons[indexPath.row]
+            
+            PokemonController.shared.deletePokemon(pokemon: pokemon)
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
@@ -45,6 +60,13 @@ class PokeListTableViewController: UITableViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "fromCell" {
+            let destinationVC = segue.destination as? PokeSearchViewController
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let pokemon = PokemonController.shared.pokemons[indexPath.row]
+            destinationVC?.pokemon = pokemon
+            
+        } 
     }
     
 
