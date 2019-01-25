b559d668f0c2bb21870034770022d50a5aef8b42
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-01-25 11:33:58 -0500

    update layout in IB

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Julian A. Fordyce on 1/25/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
