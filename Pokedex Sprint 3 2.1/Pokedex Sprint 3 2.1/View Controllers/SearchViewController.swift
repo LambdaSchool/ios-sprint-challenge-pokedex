bad3ceef6a3f502f00831229192215e92d8dfdbf
Sean Doyle <sdoylelambda@gmail.com>
2019-02-01 12:44:20 -0500

    API search outline.

--- Pokedex Sprint 3 2.1/Pokedex Sprint 3 2.1/View Controllers/SearchViewController.swift
+++ Pokedex Sprint 3 2.1/Pokedex Sprint 3 2.1/View Controllers/SearchViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    typealias completionHandler = (Error?) -> Void
     var pokemon = PokemonController.shared.pokemon
     
     @IBOutlet weak var searchPokiBar: UISearchBar!
@@ -20,14 +22,39 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
         navigationController?.popToRootViewController(animated: true)
     }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // updateviews
+        searchPokiBar.delegate = self
     }
     
+    func performSearch(for searchTerm: String, completion: @escaping completionHandler) {
+        
+        let requestURL = baseURL.appendingPathComponent(searchTerm.lowercased())
+        URLSession.shared.dataTask(with: requestURL) { (data, _, error) in
+            if let error = error {
+                NSLog("Base URL request: \(error)")
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                NSLog("Bad data request")
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            do {
+                let pokemon = try jsonDecoder.decode(Pokemon.self, from: data)
+                var typeArray: [String] = []
+                for each in pokemon(each.type.ability.name.capitalized)
+            }
+            
+            self.pokemon = pokemon
+        
+    }
 
-
-
+    }
 }
