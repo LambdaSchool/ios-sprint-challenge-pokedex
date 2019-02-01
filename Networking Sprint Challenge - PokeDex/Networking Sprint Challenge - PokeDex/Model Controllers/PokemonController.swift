ba13c145cb335704152d9f3cd56407690a89595e
Vijay Das <vijaytdas@gmail.com>
2019-02-01 10:18:13 -0600

    add skeleton files for Model and Model Controller

---
+++ Networking Sprint Challenge - PokeDex/Networking Sprint Challenge - PokeDex/Model Controllers/PokemonController.swift
@@ -0,0 +1,11 @@
+//
+//  PokemonController.swift
+//  Networking Sprint Challenge - PokeDex
+//
+//  Created by Vijay Das on 2/1/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+// file for model controller
+
+import Foundation
---
+++ Networking Sprint Challenge - PokeDex/Networking Sprint Challenge - PokeDex/Models/PokeMon.swift
@@ -0,0 +1,13 @@
+//
+//  PokeMon.swift
+//  Networking Sprint Challenge - PokeDex
+//
+//  Created by Vijay Das on 2/1/19.
+//  Copyright © 2019 Vijay Das. All rights reserved.
+//
+
+
+// file for the PokeMon model
+
+import Foundation
+
