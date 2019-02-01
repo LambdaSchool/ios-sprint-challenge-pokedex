f92279acd1b4f0e369b836a9b60615f54aa053b1
Michael R.F <mrflowers22@uwalumni.com>
2019-02-01 17:59:09 -0500

    final commit

--- w3r Pokedex Challenge/w3r Pokedex Challenge/Views/View Controllers/PokemonDetailViewController.swift
+++ w3r Pokedex Challenge/w3r Pokedex Challenge/Views/View Controllers/PokemonDetailViewController.swift
@@ -8,8 +8,16 @@
 
 import UIKit
 
-class PokemonDetailViewController: UIViewController {
-
+class PokemonDetailViewController: UIViewController, UISearchBarDelegate {
+    
+    var pC: PokemonController?
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+            print(pokemon)
+        }
+    }
+    
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -20,11 +28,62 @@ class PokemonDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let name = searchBar.text, !name.isEmpty else {return}
+        
+        pC?.fetchPokemon(with: name, completion: { (returnedPokemon, error) in
+            if let error = error {
+                print("Error fetching pokemon back from the method call: \(error.localizedDescription)")
+            }
+            
+            DispatchQueue.main.async {
+                guard let pokemon = returnedPokemon else {return}
+                self.pokemon = pokemon
+                
+            }
+        })
+        buttonProperties.setTitle("SAVE", for: .normal)
     }
     
     @IBAction func savePokemon(_ sender: UIButton) {
+        guard let pokemon = pokemon else {return}
+        pC?.savePokemon(with: pokemon)
+        navigationController?.popToRootViewController(animated: true)
+        
+    }
+    
+    func updateViews(){
+        guard let passedInPokemon = pokemon else {
+            navigationController?.title = "Search for Pokemon"
+            return
+        }
+        
+        DispatchQueue.main.async {
+            self.title = passedInPokemon.name
+            self.searchBar.isHidden = true
+            self.nameLabel.text = passedInPokemon.name
+            self.idLabel.text = String(passedInPokemon.id)
+            
+            let abilities = passedInPokemon.abilities.map({$0.ability.name}).joined(separator: ",")
+            self.abilitiesLabel.text = abilities
+            
+            let types = passedInPokemon.types.map({$0.type.name}).joined(separator: ",")
+            self.typeLabel.text = types
+            
+            
+            self.pC?.fetchSprites(with: passedInPokemon, completion: { (image, error) in
+                if let error = error {
+                    print("Error fetching UPDateViews image: \(error.localizedDescription)")
+                }
+                
+                DispatchQueue.main.async {
+                    self.imageView.image = image!
+                }
+            })
+        }
     }
 
 }
