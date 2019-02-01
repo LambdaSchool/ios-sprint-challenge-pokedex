e4b4914373315ab20abbeb00bae7c481c0a0f438
Vijay Das <vijaytdas@gmail.com>
2019-02-01 14:55:42 -0600

    add complete seacrhPokemon method to model controller

--- Networking Sprint Challenge - PokeDex/Networking Sprint Challenge - PokeDex/Model Controllers/PokemonController.swift
+++ Networking Sprint Challenge - PokeDex/Networking Sprint Challenge - PokeDex/Model Controllers/PokemonController.swift
@@ -12,19 +12,43 @@ import Foundation
 
 class PokemonController {
 
-    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
     
     var pokemonArray: [Pokemon] = []
     
-    func searchPokemon(with term: String, completion: @escaping (Pokemon?, Error?) -> Void){
-        
-        let searchURL = baseURL?.appendingPathComponent(term.lowercased())
-        
-
-        
-        
-        
-        
-    }
+    func searchPokemon(with term: String, completion: @escaping (Error?) -> Void){
+        let searchURL = baseURL.appendingPathComponent(term.lowercased())
+        URLSession.shared.dataTask(with: searchURL) { (data, _, error) in
+            if let error = error {
+                NSLog("error getting data: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                let resultPokemon = try decoder.decode(Pokemon.self, from: data)
+                
+                self.pokemonArray.append(resultPokemon)
+                completion(nil)
+            } catch {
+                print("error decoding data: \(error.localizedDescription)")
+                completion(error)
+                return
+            }
+        }.resume()
+  }
+    
+    
+ 
+    
+    
     
 }
+
+    
+
