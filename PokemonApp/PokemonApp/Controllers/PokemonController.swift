99ed4eaef833a7e2c6ee58db3c4e9b89dda216c1
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-05-10 12:44:05 -0400

    StoryBoard setup and controller finished

---
+++ PokemonApp/PokemonApp/Controllers/PokemonController.swift
@@ -0,0 +1,93 @@
+//
+//  PokemonController.swift
+//  PokemonApp
+//
+//  Created by Diante Lewis-Jolley on 5/10/19.
+//  Copyright © 2019 Diante Lewis-Jolley. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PokemonController {
+
+    private var pokemons: [Pokemon] = []
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/")!
+    var pokemon: Pokemon?
+
+
+    func create(pokemon: Pokemon) {
+        pokemons.append(pokemon)
+
+    }
+
+    func delete(pokemon: Pokemon) {
+        guard let index = pokemons.index(of: pokemon) else { return }
+        pokemons.remove(at: index)
+    }
+
+
+    func fetchPokemon(with searchTerm: String, completion: @escaping (Error?) -> Void) {
+
+        let url = baseURL.appendingPathComponent(searchTerm.lowercased())
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+
+
+
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+
+            let decoder = JSONDecoder()
+
+
+            do {
+                let pokemon = try decoder.decode(Pokemon.self, from: data)
+                self.pokemon = pokemon
+
+
+            } catch {
+                NSLog("Error decoding Pokemon: \(error)")
+                completion(nil)
+                return
+            }
+        }.resume()
+    }
+
+    func fetchImage(url: String, completion: @escaping (UIImage?, Error?) -> Void) {
+
+        let urlImage = URL(string: url)!
+
+        var request = URLRequest(url: urlImage)
+        request.httpMethod = "GET"
+
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                completion(nil, error)
+                return
+            }
+
+            guard let data = data else {
+                completion(nil, NSError())
+                return
+            }
+
+            let image = UIImage(data: data)
+            completion(image, nil)
+
+        }.resume()
+    }
+
+
+
+
+}
