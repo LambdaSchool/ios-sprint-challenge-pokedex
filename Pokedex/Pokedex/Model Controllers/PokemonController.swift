21841d64f5b2cb687a9f980e62d246fc9e906c1e
Alex <alexcc17@gmail.com>
2019-05-10 11:19:18 -0400

    Create files

---
+++ Pokedex/Pokedex/Model Controllers/PokemonController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Alex on 5/10/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Alex on 5/10/19.
+//  Copyright © 2019 Alex. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    
+    
+}
