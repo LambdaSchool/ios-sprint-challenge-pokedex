56d63bc62223a2b10ecc4e33bd529eb1e3c0931c
victor <ruizdvictor@gmail.com>
2019-05-10 12:13:53 -0400

    update ui and registered classes to storyboard views

---
+++ Sprint3/Sprint3/Views/PokemonSearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  PokemonSearchViewController.swift
+//  Sprint3
+//
+//  Created by Victor  on 5/10/19.
+//  Copyright © 2019 com.Victor. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+}
