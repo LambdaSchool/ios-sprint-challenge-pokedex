086f3dbe4cd5d922ff1b8f6e9fc90010913a03b7
Lisa Sampson <lisasampson0918@gmail.com>
2019-05-10 12:49:21 -0400

    Set up search function

--- Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -22,13 +22,65 @@ class PokemonSearchViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var abilitiesLabel: UILabel!
     @IBOutlet weak var pokemonImage: UIImageView!
     
+    // MARK: - View Loading Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        searchBar.delegate = self
+        updateViews()
     }
     
+    func updateViews() {
+        if let pokemon = pokemonController?.pokemon {
+            title = pokemon.name
+            nameLabel.text = pokemon.name
+            idLabel.text = String(pokemon.id)
+            typeLabel.text = pokemon.types.first?.type.name
+            abilitiesLabel.text = pokemon.abilities.first?.ability.name
+        } else {
+            title = "Pokemon Search"
+            nameLabel.text = "Pokemon"
+            idLabel.text = ""
+            typeLabel.text = ""
+            abilitiesLabel.text = ""
+            pokemonImage.image = nil
+            searchBar.isHidden = false
+        }
+    }
+    
+    // MARK: - Actions and Search Bar Delegate Methods
+    
     @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let pokemon = pokemonController?.pokemon else { return }
+        pokemonController?.create(pokemon: pokemon)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm  = searchBar.text, !searchTerm.isEmpty else { return }
         
+        pokemonController?.fetch(searchName: searchTerm, completion: { (error) in
+            if let error = error {
+                NSLog("There was an error fetching the data: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+            
+            self.pokemonController?.fetchImage(at: (self.pokemon?.sprites.first!.frontDefault)!, completion: { (image, error) in
+                if let error = error {
+                    NSLog("There was an error fetching the image: \(error)")
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self.pokemonImage.image = image
+                    self.updateViews()
+                }
+            })
+        })
     }
 }
