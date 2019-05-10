b734af174619b9bc118eef30d572a728bf88a12e
hectorsvill <hectorstevenvillasano@gmail.com>
2019-05-10 08:13:11 -0700

    pokeDetail pokeList

---
+++ Pokedex/Pokedex/ViewControllers/PokeDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PokeDetailViewController.swift
+//  Pokedex
+//
+//  Created by Hector Steven on 5/10/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class PokeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+}
---
+++ Pokedex/Pokedex/ViewControllers/PokeListTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PokeListTableViewController.swift
+//  Pokedex
+//
+//  Created by Hector Steven on 5/10/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class PokeListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
