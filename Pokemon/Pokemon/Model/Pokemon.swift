dc2f5b4e724133dc9909d813962e547e72ffeaf6
Michael R.F <mrflowers22@uwalumni.com>
2019-05-10 11:27:52 -0400

    implemented our data model

--- Pokemon/Pokemon/Model/Pokemon.swift
+++ Pokemon/Pokemon/Model/Pokemon.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+struct Pokemon: Codable {
+    let id: Int
+    let name: String
+    let abilities: [AbilityArray]
+    let types: [TypeArray]
+}
+
+struct AbilityArray: Codable {
+    let ability: Ability
+}
+
+struct Ability: Codable {
+    let name: String
+}
+
+struct TypeArray: Codable {
+    let type: Type
+}
+
+struct Type: Codable {
+    let name: String
+}
