4487cc169c73355d1c5dd44d2ed708762f98dcd1
Diante Lewis-Jolley <diantelewis-jolley@Diantes-MBP.frontier.com>
2019-05-10 13:58:40 -0400

    TableView Complete

--- PokemonApp/PokemonApp/View Controllers/PokemonTableViewController.swift
+++ PokemonApp/PokemonApp/View Controllers/PokemonTableViewController.swift
@@ -43,49 +43,42 @@ class PokemonTableViewController: UITableViewController {
     }
 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
-    /*
+
+
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+
+            let pokemon = pokemonController.pokemons[indexPath.row]
+            pokemonController.delete(pokemon: pokemon)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
-    }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-    }
-    */
+        if segue.identifier == "SearchSegue" {
 
+            guard let SearchVC = segue.destination as? SearchViewController else { return }
+            SearchVC.pokemonController = pokemonController
+            
+        } else if segue.identifier == "DetailSegue" {
+            guard let DetailVC = segue.destination as? SearchViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            DetailVC.pokemon = pokemonController.pokemons[indexPath.row]
+            DetailVC.pokemonController = pokemonController
+        }
+    }
 }
