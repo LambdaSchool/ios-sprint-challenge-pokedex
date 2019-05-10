b06bc2d4a4dbb487051823fe8d42d76c2a556841
Dan Morse <morsedan@outlook.com>
2019-05-10 09:20:59 -0700

    started view controllers

---
+++ Pokedex/Pokedex/View Controllers/CaracterTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  CaracterTableViewController.swift
+//  Pokedex
+//
+//  Created by morse on 5/10/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class CaracterTableViewController: UITableViewController {
+    
+    private let characterController = CharacterController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return characterController.characters.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CharacterCell", for: indexPath)
+        
+        let character = characterController.characters[indexPath.row]
+        cell.textLabel?.text = character.name
+
+        return cell
+    }
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SearchSegue" {
+            if let detailVC = segue.destination as? DetailViewController {
+                
+            }
+        }
+        
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
