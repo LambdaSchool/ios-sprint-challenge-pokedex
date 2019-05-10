0d88fe3344635a290163d3595800034bbac76b96
victor <ruizdvictor@gmail.com>
2019-05-10 12:26:18 -0400

    connected outlets to class

--- Sprint3/Sprint3/Views/PokemonSearchViewController.swift
+++ Sprint3/Sprint3/Views/PokemonSearchViewController.swift
@@ -11,11 +11,21 @@ import UIKit
 
 class PokemonSearchViewController: UIViewController {
     
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var pokemonId: UILabel!
+    @IBOutlet weak var pokemonTypes: UILabel!
+    @IBOutlet weak var pokemonAbilities: UILabel!
+    @IBOutlet weak var pokemonImage: UIImageView!
+    @IBOutlet weak var saveButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        
+    }
     
     
     
