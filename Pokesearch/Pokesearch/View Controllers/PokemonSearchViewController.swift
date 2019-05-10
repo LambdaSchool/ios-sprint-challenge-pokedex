85e9c1563f9449d320f22d95c2af209a226cdafe
Michael Redig <mredig@gmail.com>
2019-05-10 11:52:42 -0500

    connected a bunch of outlets in search view set up a lot of search view stuff

--- Pokesearch/Pokesearch/View Controllers/PokemonSearchViewController.swift
+++ Pokesearch/Pokesearch/View Controllers/PokemonSearchViewController.swift
@@ -9,12 +9,53 @@
 import UIKit
 
 class PokemonSearchViewController: UIViewController {
+	@IBOutlet var pokemonSearchBar: UISearchBar!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+	@IBOutlet var pokemonNameLabel: UILabel!
+	@IBOutlet var pokemonIDLabel: UILabel!
+	@IBOutlet var primaryStackView: UIStackView!
+	@IBOutlet var typeLabel: UILabel!
+	@IBOutlet var abilitiesLabel: UILabel!
+	@IBOutlet var throwPokeballButton: UIButton!
 
-        // Do any additional setup after loading the view.
-    }
+	var pokemonController: PokemonController?
+	var pokemon: Pokemon? {
+		didSet {
+			updateViews()
+		}
+	}
+	private var spritesURLs: [String]?
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		showViews(show: false)
+		updateViews()
+	}
+
+	func updateViews() {
+		guard let pokemon = pokemon, isViewLoaded else { return }
+		showViews(show: true)
+		pokemonNameLabel.text = pokemon.name
+		pokemonIDLabel.text = "\(pokemon.id)"
+		typeLabel.text = pokemon.types.reduce ("") { $0 + $1.type.name + " " }
+		abilitiesLabel.text = pokemon.abilities.reduce("") { $0 + $1.ability.name + " " }
+		spritesURLs = pokemon.sprites.compactMap { $0.value }.sorted()
+	}
+
+	func showViews(show: Bool, hideButton: Bool = false) {
+		primaryStackView.isHidden = !show
+		pokemonNameLabel.isHidden = !show
+		pokemonIDLabel.isHidden = !show
+		throwPokeballButton.isEnabled = show
+		throwPokeballButton.isHidden = hideButton
+	}
+
+
+	@IBAction func throwPokeballPressed(_ sender: UIButton) {
+		guard let pokemon = pokemon else { return }
+		pokemonController?.catchPokemon(pokemon)
+		navigationController?.popViewController(animated: true)
+	}
 }
 
 
