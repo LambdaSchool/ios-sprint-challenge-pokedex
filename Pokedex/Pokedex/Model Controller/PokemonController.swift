5d18d881a70d038de3a4ca2f238f92ac23f0c1a5
Christopher Aronson <christopheraronson@icloud.com>
2019-05-10 10:52:46 -0700

    Xcode crashed and I lost a lot of work.

---
+++ Pokedex/Pokedex/Model Controller/PokemonController.swift
@@ -0,0 +1,29 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Christopher Aronson on 5/10/19.
+//  Copyright © 2019 Christopher Aronson. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
+
+class PokemonController {
+    
+    private let baseUrl = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    
+}
--- Pokedex/Pokedex/Model/Pokemon.swift
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -5,10 +5,9 @@
 //  Created by Christopher Aronson on 5/10/19.
 //  Copyright © 2019 Christopher Aronson. All rights reserved.
 //
-
 import Foundation
 
-struct Welcome: Codable {
+struct Pokemon: Codable {
     let id: Int
     let name: String
     let abilities: [AbilityElement]
@@ -17,21 +16,39 @@ struct Welcome: Codable {
 }
 
 struct AbilityElement: Codable {
+    let isHidden: Bool
+    let slot: Int
     let ability: TypeClass
+    
+    enum CodingKeys: String, CodingKey {
+        case isHidden = "is_hidden"
+        case slot, ability
+    }
 }
 
 struct TypeClass: Codable {
     let name: String
+    let url: String
 }
 
 struct Sprites: Codable {
-    let frontDefault: String
+    let backFemale, backShinyFemale, backDefault, frontFemale: String
+    let frontShinyFemale, backShiny, frontDefault, frontShiny: String
     
     enum CodingKeys: String, CodingKey {
+        case backFemale = "back_female"
+        case backShinyFemale = "back_shiny_female"
+        case backDefault = "back_default"
+        case frontFemale = "front_female"
+        case frontShinyFemale = "front_shiny_female"
+        case backShiny = "back_shiny"
         case frontDefault = "front_default"
+        case frontShiny = "front_shiny"
     }
 }
 
 struct TypeElement: Codable {
+    let slot: Int
     let type: TypeClass
 }
+
