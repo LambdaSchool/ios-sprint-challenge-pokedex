3a938c2221ac4fc083363d6635a5a70d4a382f9f
Taylor Lyles <hawks93187@gmail.com>
2019-05-17 08:51:43 -0700

    Added baser and empty array, network errors and httpmethod. search function

--- Pokedex/Pokedex/Model Controller/PokemonController.swift
+++ Pokedex/Pokedex/Model Controller/PokemonController.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+	case get = "GET"
+	case put = "PUT"
+	case post = "POST"
+	case delete = "DELETE"
+}
+
+enum NetworkError: Error {
+	case badResponse
+	case otherError
+	case badData
+	case noDecode
+}
+
+class PokemonController {
+	var pokemon: [Pokemon] = []
+	private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+	
+	func search(for poke: String, completion: @escaping (Result<Pokemon, NetworkError>) -> Void) {
+		let url = baseURL.appendingPathComponent(poke)
+		
+		var request = URLRequest(url: url)
+		request.httpMethod = HTTPMethod.get.rawValue
+		
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				print("Bad Response: \(response.statusCode)")
+				completion(.failure(.badResponse))
+				return
+			}
+			
+			if let error = error {
+				NSLog("Error: \(error)")
+				completion(.failure(.otherError))
+				return
+			}
+			
+			guard let data = data else {
+				NSLog("Data error")
+				completion(.failure(.badData))
+				return
+			}
+			
+			let decoder = JSONDecoder()
+			do {
+				let search = try decoder.decode(Pokemon.self, from: data)
+				completion(.success(search))
+			} catch {
+				NSLog("Decoding error: \(error)")
+				completion(.failure(.noDecode))
+				return
+			}
+		}.resume()
+		
+		
+		
+	}
+}
