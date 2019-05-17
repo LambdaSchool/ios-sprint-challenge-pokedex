74dc8d77d1590f6c46afddb3d32aa25081ec43be
Taylor Lyles <hawks93187@gmail.com>
2019-05-17 09:45:59 -0700

    TVC: viewwillappear, cellforrow, prepare for segue, delete

--- Pokedex/Pokedex/View Controller/PokemonTableViewController.swift
+++ Pokedex/Pokedex/View Controller/PokemonTableViewController.swift
@@ -9,23 +9,57 @@
 import UIKit
 
 class PokemonTableViewController: UITableViewController {
-
+	var pokemonController = PokemonController()
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		tableView.reloadData()
+	}
 
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		
-        return 0
+        return pokemonController.pokemon.count
     }
 	
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Pokemoncell", for: indexPath)
+		let currentPokemon = pokemonController.pokemon[indexPath.row]
+		
+		cell.textLabel?.text = currentPokemon.name
+
+		return cell
 		
 		
 	}
 
+	
+	override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+		if editingStyle == .delete {
+			pokemonController.delete(indexOfPoke: indexPath)
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		} else if editingStyle == .insert {
+		}
+	}
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "SearchShow" {
+			let searchVC = segue.destination as? SearchViewController
+			searchVC?.pokemonController = pokemonController
+		}
+		else if segue.identifier == "DetailShow" {
+			if let indexPath = self.tableView.indexPathForSelectedRow {
+			let detailVC = segue.destination as? SearchViewController
+			detailVC?.pokemonController = pokemonController
+			detailVC?.pokemon = pokemonController.pokemon[indexPath.row]
+		}
+	}
+	}
 	
 }
