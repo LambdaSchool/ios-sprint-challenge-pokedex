68ea23eec2dac42f114423c2fca0eb52a14b1fc7
Ryan Murphy <thomasryanmurphy@gmail.com>
2019-05-17 12:00:13 -0400

    Sort of built the mainStoryBoard ... could use some work.

---
+++ PokemonChallenge/PokemonChallenge/PokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTableViewCell.swift
+//  PokemonChallenge
+//
+//  Created by Ryan Murphy on 5/17/19.
+//  Copyright © 2019 Ryan Murphy. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
