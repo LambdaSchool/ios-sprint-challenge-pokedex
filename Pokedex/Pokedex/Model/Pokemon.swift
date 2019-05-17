126eec1eaa78a5cdd0f9206939d1290ac655722f
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-17 09:36:06 -0600

    UI configured
    Created a TVC for listing the Pokemon, and created two VC, one for searching Pokemon and another for diplaying Pokemon details from the TVC cell

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 5/17/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import Foundation
---
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by Mitchell Budge on 5/17/19.
+//  Copyright © 2019 Mitchell Budge. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonIDLabel: UILabel!
+    @IBOutlet weak var pokemonTypeLabel: UILabel!
+    @IBOutlet weak var pokemonAbilityLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
