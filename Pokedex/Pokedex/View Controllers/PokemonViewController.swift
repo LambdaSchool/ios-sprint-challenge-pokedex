0a4aa95962c0ccf9e4127d7f38f55ad59947e568
Jeremy Taylor <jtaylor@jctaylor.org>
2019-05-17 10:52:33 -0500

    Storyboard setup done

---
+++ Pokedex/Pokedex/View Controllers/PokemonViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Jeremy Taylor on 5/17/19.
+//  Copyright © 2019 Bytes-Random L.L.C. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonIDLabel: UILabel!
+    @IBOutlet weak var pokemonTypesLabel: UILabel!
+    @IBOutlet weak var pokemonAbilitiesLabel: UILabel!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
