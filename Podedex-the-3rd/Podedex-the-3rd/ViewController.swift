436e3f2864b0faad9a2860ef5f3e1680791be671
Paul Solt <PaulSolt@gmail.com>
2019-05-17 15:52:59 -0400

    3rd attempt reusing 2nd attempt project, created new project

---
+++ Podedex-the-3rd/Podedex-the-3rd/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Podedex-the-3rd
+//
+//  Created by Paul Solt on 5/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
