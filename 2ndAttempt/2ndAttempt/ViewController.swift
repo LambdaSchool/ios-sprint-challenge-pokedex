08c0279147ebf6e94879140403af58601545c753
Paul Solt <PaulSolt@gmail.com>
2019-05-17 11:58:15 -0400

    Added 2nd attempt.

---
+++ 2ndAttempt/2ndAttempt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  2ndAttempt
+//
+//  Created by Paul Solt on 5/17/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
