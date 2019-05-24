1e24a15259b63814b7bf015179e8827597a03d07
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-24 12:21:23 -0400

    View Controller
    view controller implementation and wiring all my outlets and functions to pass information

--- Pokedex/Pokedex/View Controllers/SearchPokemonViewController.swift
+++ Pokedex/Pokedex/View Controllers/SearchPokemonViewController.swift
@@ -8,23 +8,87 @@
 
 import UIKit
 
-class SearchPokemonViewController: UIViewController {
+class SearchPokemonViewController: UIViewController, UISearchBarDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        updateViews()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Actions
+    
+    @IBAction func save(_ sender: Any) {
     }
-    */
+    
+    
+    //MARK: - Functions
+    
+    func updateViews() {
+        guard let pokemon = pokemon, isViewLoaded else {
+            title = "Pokemon Search"
+            return }
+        
+        title = pokemon.name.capitalized
+        nameLabel.text = pokemon.name.capitalized
+        idLabel.text = "ID: \(pokemon.id)"
+        
+        let type = pokemon.types.map { $0.type.name }.joined(separator: ", ")
+        typesLabel.text = "Type(s): \((type).capitalized)"
+        
+        let abilities = pokemon.abilities.map { $0.ability.name }.joined(separator: ", ")
+        abilitiesLabel.text = "Abilites: \((abilities).capitalized)"
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let name = searchBar.text, !name.isEmpty else { return }
+        
+        pokemonController?.fetchPokemon(with: name, completion: { (returnedPokemon, error) in
+            if let error = error {
+                NSLog("Error fetching pokemon: \(error)")
+                return
+            }
+            
+            if let pokemon = returnedPokemon {
+                DispatchQueue.main.async {
+                    self.pokemon = pokemon
+                }
+                
+                self.pokemonController?.fetchSprite(with: pokemon, completion: { (image, error) in
+                    if let error = error {
+                        NSLog("Error retrieving image: \(error)")
+                        return
+                    }
+                    
+                    if let image = image {
+                        DispatchQueue.main.async {
+                            self.imageView.image = image
+                            self.saveButton.setTitle("Save \(pokemon.name.capitalized)", for: .normal)
+                        }
+                    }
+                })
+            }
+        })
+    }
+    
+    
+    //MARK: - Properties
+    
+    var pokemonController: PokemonController?
+    
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
 }
