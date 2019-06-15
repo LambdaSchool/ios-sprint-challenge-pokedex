9d503be2b8749faf435e472ef4766fb595fc2ca2
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-06-15 15:03:27 -0400

    Inital

---
+++ Pokedex (Sprint)/Pokedex (Sprint)/Controllers/PokemonController.swift
@@ -0,0 +1,32 @@
+//
+//  PokemonController.swift
+//  Pokedex (Sprint)
+//
+//  Created by Nathan Hedgeman on 6/15/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import Foundation
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+class PokemonController {
+    
+    //Properties
+    var pokemonSearchResult: [Pokemon] = []
+    var pokemonList: [Pokemon] = []
+    
+    
+    
+    func getPokemon() {
+        //MARK: Fetch pokemon data
+    }
+    
+    func getPokemonImage() {
+        //Fetch Image
+    }
+    
+}
---
+++ Pokedex (Sprint)/Pokedex (Sprint)/Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex (Sprint)
+//
+//  Created by Nathan Hedgeman on 6/15/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
