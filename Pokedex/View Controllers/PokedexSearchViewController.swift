6ffcd303e5b3905dc737065e6e60df7504a6a78c
Kat Milton <mltnkat@gmail.com>
2019-06-21 09:38:04 -0700

    Updated Search View Controller.

--- Pokedex/View Controllers/PokedexSearchViewController.swift
+++ Pokedex/View Controllers/PokedexSearchViewController.swift
@@ -9,15 +9,55 @@
 import UIKit
 
 class PokedexSearchViewController: UIViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var pokemonNameLabel: UILabel!
+    @IBOutlet var pokemonSprite: UIImageView!
+    @IBOutlet var pokemonIDLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var abilitiesLabel: UILabel!
+    @IBOutlet var saveButton: UIButton!
 
     var pokemonController = PokemonController()
     
+    var pokemon: Pokemon? {
+        didSet {
+            DispatchQueue.main.async {
+                self.updateViews()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        pokemonNameLabel.text = ""
+        saveButton.setTitle("", for: .normal)
 
         // Do any additional setup after loading the view.
     }
     
+    func updateViews() {
+        guard let pokemon = pokemon else { return }
+        pokemonNameLabel.text = pokemon.name.capitalized
+        let id = String(pokemon.id)
+        pokemonIDLabel.text = "ID: \(id)"
+        let type: [String] = pokemon.types.map { $0.type.name }
+        typeLabel.text = "Type(s): \(type.joined(separator:", "))"
+        let abilities: [String] = pokemon.abilities.map { $0.ability.name }
+        abilitiesLabel.text = "Abilities: \(abilities.joined(separator: "\n"))"
+        guard let url = URL(string: pokemon.sprites.front_default),
+            let spriteData = try? Data(contentsOf: url) else { return }
+        pokemonSprite.image = UIImage(data: spriteData)
+        saveButton.setTitle("Save Pokemon", for: .normal)
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIButton) {
+        guard let pokemon = pokemon else { return }
+        pokemonController.savePokemon(pokemon: pokemon)
+        navigationController?.popViewController(animated: true)
+        print("Saving pokemon to list")
+    }
 
     /*
     // MARK: - Navigation
@@ -29,4 +69,22 @@ class PokedexSearchViewController: UIViewController {
     }
     */
 
+}
+
+extension PokedexSearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchTerm = searchBar.text else { return }
+        print("Searching")
+        
+        pokemonController.fetchPokemon(for: searchTerm) { (result, error) in
+            if error != nil {
+                NSLog("Error searching for: \(String(describing: error))")
+            }
+            self.pokemon = result
+            DispatchQueue.main.async {
+                self.updateViews()
+                self.searchBar.text = ""
+            }
+        }
+    }
 }
--- Pokedex/Views/PokemonTableViewCell.swift
+++ Pokedex/Views/PokemonTableViewCell.swift
@@ -13,10 +13,7 @@ class PokemonTableViewCell: UITableViewCell {
     @IBOutlet var pokemonNameLabel: UILabel!
     @IBOutlet var pokemonSprite: UIImageView!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
