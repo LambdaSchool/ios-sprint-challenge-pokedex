f7038677a124a7ab666b8d8faf4d411f9560138a
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-09 13:46:51 -0400

    View Controllers Created

---
+++ Pokedex/Pokedex/New Group/PokemonSearchViewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonSearchViewViewController.swift
+//  Pokedex
+//
+//  Created by Nathan Hedgeman on 8/9/19.
+//  Copyright © 2019 Nate Hedgeman. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
