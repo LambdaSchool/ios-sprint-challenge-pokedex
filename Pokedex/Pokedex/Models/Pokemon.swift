65989aed9cb5fd8fcd738b042abf2ca444240782
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-09 13:49:10 -0400

    Fixing decoding bug

--- Pokedex/Pokedex/Models/Pokemon.swift
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -11,10 +11,25 @@ import Foundation
 struct Pokemon: Codable {
 	let id: Int
 	let name: String
-	let abilities: [KeyString]
-	let types: [KeyString]
+	let abilities: [Ability]
+	let types: [Type]
+	let sprites: Sprite
+}
+
+struct Ability: Codable {
+	let ability: KeyString
+}
+
+struct Type: Codable {
+	let type: KeyString
+}
+
+struct Sprite: Codable {
+	let frontDefault: URL
+	let backDefault: URL
 }
 
 struct KeyString: Codable {
 	let name: String
+	let url: URL
 }
