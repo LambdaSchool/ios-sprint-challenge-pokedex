d9c7badfda472d2d7717b7e7fb4bd4992b1c9e6c
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-08-09 09:30:52 -0700

    Search for Pokemon request and response

--- Pokedex/Pokedex/Model Controller/PokemonController.swift
+++ Pokedex/Pokedex/Model Controller/PokemonController.swift
@@ -16,9 +16,41 @@ class PokemonController {
 	private let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
 	
 	func searchPokemon(for pokemon: String, completion: @escaping (Result<Pokemon, NetworkError>) -> Void ) {
+		let searchURL = baseURL.appendingPathComponent("poke")
 		
+		var request = URLRequest(url: searchURL)
+		request.httpMethod = HTTPMethod.get.rawValue
+		
+		URLSession.shared.dataTask(with: request) { (data, response, error) in
+			if let response = response as? HTTPURLResponse,
+				response.statusCode != 200 {
+				(NSLog("Bad Response: \(response.statusCode)"))
+				completion(.failure(.badResponse))
+				return
+			}
+			
+			if let error = error {
+				NSLog("Error: \(error)")
+				completion(.failure(.otherError))
+				return
+			}
+			
+			guard let data = data else {
+				NSLog("Error: Bad Data)")
+				completion(.failure(.badData))
+				return
+			}
+			
+			do {
+				let search = try JSONDecoder().decode(Pokemon.self, from: data)
+				completion(.success(search))
+			} catch {
+				NSLog("Decoding error: \(error)")
+				completion(.failure(.noDecode))
+				return
+			}
+		}.resume()
 	}
-	
 }
 
 enum HTTPMethod: String {
