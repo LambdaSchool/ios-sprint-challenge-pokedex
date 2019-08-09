fc17f9e69687d8efc73097744954278fa961cf3b
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-08-09 10:32:35 -0700

    Added searchFor function and updateView function in VC

--- Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
 	
+	var pokemonController: PokemonController?
+	var pokemon: Pokemon?
+	
 	@IBOutlet weak var pokemonSearchBar: UISearchBar!
 	@IBOutlet weak var nameLabel: UILabel!
 	@IBOutlet weak var pokemonImageView: UIImageView!
@@ -21,7 +24,46 @@ class PokemonDetailViewController: UIViewController {
         super.viewDidLoad()
     }
 	
+	func searchClicked(_ searchBar: UISearchBar) {
+		guard let text = pokemonSearchBar.text?.lowercased() else { return }
+		guard let pokeController = pokemonController else { return }
+		
+		pokemonController?.searchPokemon(for: text) { (result) in
+			
+			do {
+				let thisPokemon = try result.get()
+				DispatchQueue.main.async {
+					self.updateView(with: thisPokemon)
+				}
+				
+				pokeController.getImage(at: thisPokemon.sprites.fontDefault) { (result) in
+					
+					do {
+						let image = try result.get()
+						DispatchQueue.main.async {
+							self.pokemonImageView.image = image
+						}
+					} catch {
+						NSLog("Could not load image")
+					}
+				}
+				self.pokemon = thisPokemon
+			} catch {
+				NSLog("Error: \(error)")
+			}
+		}
+	}
+	
+	func updateView(with searchedPokemon: Pokemon) {
+		nameLabel.text = searchedPokemon.name
+		pokemonIdLabel.text = String(searchedPokemon.Id)
+		pokemonTypeLabel.text = searchedPokemon.type[0].type
+		pokemonAbilitiesLabel.text = searchedPokemon.abilities[0].abilities
+	}
+	
 	@IBAction func savePokemonButton(_ sender: UIButton) {
 	}
+	
+	
 	
 }
