22f5bfb24173e15f61e3e9f63b90ca73572f4247
Nate Hedgeman <nate.hedgeman@gmail.com>
2019-08-09 14:05:31 -0400

    MVP

--- Pokedex/Pokedex/New Group/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/New Group/PokemonDetailViewController.swift
@@ -10,24 +10,49 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
     
+    //Propeties
     var pokemonController: PokemonController?
     var pokemon: Pokemon?
-
+    
+    @IBOutlet weak var pokemonImage: UIImageView!
+    
+    //Labels
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var idDetailsLabel: UILabel!
+    
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var typeDetailsLabel: UILabel!
+    
+    @IBOutlet weak var abilityLabel: UILabel!
+    @IBOutlet weak var abilityDetailsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let pokemon = pokemon else { return }
+        updateViews(with: pokemon)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews(with pokemon: Pokemon) {
+        
+        title = pokemon.name
+        
+        self.idLabel.text = "ID:"
+        self.idDetailsLabel.text = "\(pokemon.id)"
+        
+        self.typesLabel.text = "Type:"
+        self.typeDetailsLabel.text = pokemon.types.first?.type.name
+        
+        self.abilityLabel.text = "Abilities:"
+        self.abilityDetailsLabel.text = pokemon.abilities.first?.ability.name
+        
+        pokemonController?.fetchImage(at: pokemon.sprites.frontDefault, completion: { (imageResult) in
+            if let image = try? imageResult.get() {
+                DispatchQueue.main.async {
+                    self.pokemonImage.image = image
+                }
+                
+            }
+        })
     }
-    */
-
 }
