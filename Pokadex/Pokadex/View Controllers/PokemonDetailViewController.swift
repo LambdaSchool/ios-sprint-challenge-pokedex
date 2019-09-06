1aa5ae72a23acb34a9c877adf18ec34fa38fa0be
bfvilchez <bfvilchez@icloud.com>
2019-09-06 09:28:00 -0600

    created view Controllers and linked them to the storyboard files

---
+++ Pokadex/Pokadex/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokadex
+//
+//  Created by brian vilchez on 9/6/19.
+//  Copyright © 2019 brian vilchez. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
