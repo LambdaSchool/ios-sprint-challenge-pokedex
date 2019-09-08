d75cecfa6ba7fcf6db2ba644a5b91879aa3a60c3
AustinPotts <pottsevolvements@gmail.com>
2019-09-07 23:34:21 -0400

    MVP Reached for Sprint
    Detail View Configured for Types, ID

--- Pokedex-v3 Lambda/View Controllers/PokemonDetailViewController.swift
+++ Pokedex-v3 Lambda/View Controllers/PokemonDetailViewController.swift
@@ -13,6 +13,9 @@ class PokemonDetailViewController: UIViewController {
     
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pokemonName: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
     
     
     var pokemonController: PokemonController?
@@ -34,7 +37,19 @@ class PokemonDetailViewController: UIViewController {
             let pokemon = pokemon else{return}
         guard let pokemonImageData = try? Data(contentsOf: pokemon.sprites.frontDefault) else {return}
         imageView.image = UIImage(data: pokemonImageData)
+        pokemonName.text = pokemon.name.capitalized
+        idLabel.text = "ID: \(pokemon.id)"
         title = pokemon.name
+        
+        var types = ""
+        let typeArray = pokemon.types
+        
+        for type in typeArray {
+            types.append("Type: \(type.type.name.capitalized)")
+            
+        }
+        
+        typeLabel.text = types
         
     }
 
