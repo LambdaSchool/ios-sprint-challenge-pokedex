40eaaf1bb7ae6d365eed2bea2598ecbce3810408
John Kouris <jdkouris@gmail.com>
2019-09-14 09:43:12 -0700

    Created necessary code files for tableView and detailView

---
+++ Pokedex/Pokedex/View Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by John Kouris on 9/14/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+    
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var savePokemonButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func savePokemonButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+
+}
