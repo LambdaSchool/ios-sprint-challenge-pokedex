ce3e2cf1a418c6483408b4a775d104783966d98b
Wilma <kima_92@fastmail.com>
2019-10-04 12:00:38 -0400

    Set up Storyboard, and PokedexDetailVC outlets

---
+++ Pokedex/View Controllers/PokedexDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  PokedexDetailViewController.swift
+//  Pokedex
+//
+//  Created by macbook on 10/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PokedexDetailViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
