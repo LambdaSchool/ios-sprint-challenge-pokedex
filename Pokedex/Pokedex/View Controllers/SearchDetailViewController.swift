57e9c270ccb1ae4f4288eb0209e1c4900fd51fcf
Gi Pyo Kim <gipyok@gmail.com>
2019-10-04 08:46:59 -0700

    baisc UI and class setup

---
+++ Pokedex/Pokedex/View Controllers/SearchDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchDetailViewController.swift
+//  Pokedex
+//
+//  Created by Gi Pyo Kim on 10/4/19.
+//  Copyright © 2019 GIPGIP Studio. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var pokeNameLabel: UILabel!
+    @IBOutlet weak var pokeImageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
