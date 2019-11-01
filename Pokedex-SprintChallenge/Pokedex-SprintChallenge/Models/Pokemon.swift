49ca03a3b37f2eaf87cd94ce50e3b41cd389b969
Dan M <djmthedev@gmail.com>
2019-11-01 08:44:22 -0700

    added Pokemon Model

---
+++ Pokedex-SprintChallenge/Pokedex-SprintChallenge/Models/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex-SprintChallenge
+//
+//  Created by morse on 11/1/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Decodable {
+    let name: String
+}
