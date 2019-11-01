06ed304112cea6dbf8537a54341d5f0759545e30
Dan M <djmthedev@gmail.com>
2019-11-01 08:45:14 -0700

    added PokemonDetailViewController

---
+++ Pokedex-SprintChallenge/Pokedex-SprintChallenge/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex-SprintChallenge
+//
+//  Created by morse on 11/1/19.
+//  Copyright © 2019 morse. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
