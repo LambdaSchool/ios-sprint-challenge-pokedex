74a97d7bf6130f18ccb3dde848a1c830bc4ba1fe
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-11-01 14:09:37 -0400

    commit 2: API, models, testing

---
+++ Pokédex/Pokédex/PokemonAPIController.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonAPIController.swift
+//  Pokedex
+//
+//  Created by Thomas Sabino-Benowitz on 11/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Pokédex/Pokédex/Pokémon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokémon.swift
+//  Pokédex
+//
+//  Created by Thomas Sabino-Benowitz on 11/1/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
