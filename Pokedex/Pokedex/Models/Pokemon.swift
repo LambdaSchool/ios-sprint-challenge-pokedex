024d80bb70c4067c47a13ea9aa9a9bf0bcbba60d
Brandi <blubrandi0@gmail.com>
2019-11-01 10:09:29 -0600

    Planned out storyboard with added detailvc and search vc.  Added appropriate elements to each scene.

---
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Brandi on 11/1/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+    let id: Int
+    let abilities: [String]
+    let type: String
+}
