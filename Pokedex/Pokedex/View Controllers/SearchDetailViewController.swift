cce2e6616b7d02fb4fb810f2373b333dd3ebc732
Jon Bash <jonbashmusic@gmail.com>
2019-11-01 09:39:24 -0700

    update detail views ()

--- Pokedex/Pokedex/View Controllers/SearchDetailViewController.swift
+++ Pokedex/Pokedex/View Controllers/SearchDetailViewController.swift
@@ -9,22 +9,48 @@
 import UIKit
 
 class SearchDetailViewController: UIViewController {
-
+    
+    var pokemon: Pokemon?
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pokemonIDLabel: UILabel!
+    @IBOutlet weak var pokemonTypesLabel: UILabel!
+    @IBOutlet weak var pokemonAbilitiesLabel: UILabel!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        if let pokemon = pokemon {
+            nameLabel.text = pokemon.name
+            pokemonIDLabel.text = "\(pokemon.id)"
+            pokemonTypesLabel.text = pokemon.types.joined(separator: ", ")
+            pokemonAbilitiesLabel.text = pokemon.abilities.joined(separator: ", ")
+            showPokemonDetails(true)
+        } else {
+            showPokemonDetails(false)
+        }
     }
-    */
 
+    func showPokemonDetails(_ shouldShow: Bool) {
+        nameLabel.isHidden = !shouldShow
+        imageView.isHidden = !shouldShow
+        pokemonIDLabel.isHidden = !shouldShow
+        pokemonTypesLabel.isHidden = !shouldShow
+        pokemonAbilitiesLabel.isHidden = !shouldShow
+        
+        idLabel.isHidden = !shouldShow
+        typesLabel.isHidden = !shouldShow
+        abilitiesLabel.isHidden = !shouldShow
+    }
 }
