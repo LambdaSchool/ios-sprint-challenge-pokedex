fa2e375ba2b2e3a992521a5ee578045772fd176b
Alex Thompson <guitarman226@gmail.com>
2019-11-09 12:27:39 -0600

    Created storyBoard created model files and Cocoa Touch classed for storyboard

---
+++ Pokedex/Pokedex/Controllers/PokemonSearchViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonSearchViewController.swift
+//  Pokedex
+//
+//  Created by Alex Thompson on 11/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class PokemonSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pokedex/Pokedex/Pokemon Models/Pokemon.swift
@@ -0,0 +1,9 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Alex Thompson on 11/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
