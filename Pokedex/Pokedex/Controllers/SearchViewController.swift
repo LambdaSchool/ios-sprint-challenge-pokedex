eac31dd6d222b63e589ae06fbf8e78b4317e97bc
Aaron Haughton <dreamcast2000@outlook.com>
2019-11-12 00:34:05 -0500

    Fixed abilities and types to display more than one. "Gotta Catch'em all!"

--- Pokedex/Pokedex/Controllers/SearchViewController.swift
+++ Pokedex/Pokedex/Controllers/SearchViewController.swift
@@ -23,6 +23,7 @@ class SearchViewController: UIViewController {
     var delegate: [Pokemon]?
     var newPokemon: Pokemon?
     var apiController = APIController()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,23 +65,43 @@ class SearchViewController: UIViewController {
     func updateViews() {
         
         if let newPokemon = apiController.myPokemon {
+          
             let id = String(newPokemon.id)
+            let abilityCount = newPokemon.abilities.count
             guard let ability = newPokemon.abilities[0].ability?.name else { return }
-            let typecount = newPokemon.types?.count
-            guard let type = newPokemon.types?[0].type?.name else { return }
-            self.idLabel.text = "ID: \(id)"
-            self.nameLabel.text = newPokemon.name
-            self.typesLabel.text = "Type: \(type)"
-            self.abilitiesLabel.text = "Abilites: \(ability)"
-            guard let image = newPokemon.sprites?.frontDefault else {return}
-            //Image
-            let url = URL(string: image)!
-            let data = try? Data(contentsOf: url)
-            
-            if let imageData = data {
-                self.pokemonImage.image = UIImage(data: imageData)
+            var allAbilities = ""
+            for number in 0...abilityCount-1 {
+                if abilityCount == 1 {
+                    allAbilities = ability
+                    break
+                }
+                guard let addedAbility = newPokemon.abilities[number].ability?.name else { return }
+                allAbilities = "\(ability), \(addedAbility)"
             }
-            
+            guard let typecount = newPokemon.types?.count else {return}
+            guard let type = newPokemon.types?[0].type?.name else { return }
+            var newType = ""
+            for number in 0...typecount-1 {
+                if typecount == 1 {
+                    newType = type
+                    break}
+                guard let addedType = newPokemon.types?[number].type?.name else {return}
+                newType = "\(type), \(addedType)"
+            }
+            DispatchQueue.main.async {
+                self.idLabel.text = "ID: \(id)"
+                self.nameLabel.text = newPokemon.name
+                self.typesLabel.text = "Type: \(newType)"
+                self.abilitiesLabel.text = "Abilites: \(allAbilities)"
+                guard let image = newPokemon.sprites?.frontDefault else {return}
+                //Image
+                let url = URL(string: image)!
+                let data = try? Data(contentsOf: url)
+                
+                if let imageData = data {
+                    self.pokemonImage.image = UIImage(data: imageData)
+                }
+            }
         }
     }
 }
