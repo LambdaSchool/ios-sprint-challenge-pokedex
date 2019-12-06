99feebe468270e471fd6e1a279289fdba815e1c1
Patrick Millet <lohs79seahawk@gmail.com>
2019-12-06 11:41:27 -0600

    finished Detail View and table view controller. MVP working with stretch

---
+++ iOS12-Pokedex/iOS12-Pokedex/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  iOS12-Pokedex
+//
+//  Created by Patrick Millet on 12/6/19.
+//  Copyright © 2019 Patrick Millet. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
