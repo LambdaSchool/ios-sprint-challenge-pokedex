f8a843817082c1e79ba4b864797ee2bbc2de10f8
Tyler Christian <tylerc71197@gmail.com>
2019-12-06 12:28:44 -0500

    storyboard and models created

---
+++ PokedexApp/PokedexApp/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  PokedexApp
+//
+//  Created by Lambda_School_Loaner_218 on 12/6/19.
+//  Copyright © 2019 Lambda_School_Loaner_218. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+   let name: String
+   let id: Int
+   let sprite: Sprite
+   let type: [Type]
+}
+
+struct Sprite: Codable  {
+    let frontDefault: URL
+}
+
+struct Type: Codable, Equatable {
+    let type: Species
+}
+
+struct Species: Equatable, Codable {
+    let name: String
+}
