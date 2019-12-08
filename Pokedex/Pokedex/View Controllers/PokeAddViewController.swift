001ac0679f5213b868692a39bc6f8cf32a4643e5
Alexander Supe <alex.m.supe@gmail.com>
2019-12-08 17:43:45 +0100

    Added Views/VCs

---
+++ Pokedex/Pokedex/View Controllers/PokeAddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokeAddViewController.swift
+//  Pokedex
+//
+//  Created by Alexander Supe on 12/8/19.
+//  Copyright © 2019 Alexander Supe. All rights reserved.
+//
+
+import UIKit
+
+class PokeAddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pokedex/Pokedex/View Controllers/PokeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokeTableViewCell.swift
+//  Pokedex
+//
+//  Created by Alexander Supe on 12/8/19.
+//  Copyright © 2019 Alexander Supe. All rights reserved.
+//
+
+import UIKit
+
+class PokeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
