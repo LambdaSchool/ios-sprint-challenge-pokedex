fc4b7e35ac780a06b3c5e3e334025f6a65e970f0
Tobi Tselios <tobi.tselios@gmail.com>
2020-01-17 19:59:39 +0000

    Pokemon model created

---
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -0,0 +1,41 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Tobi Kuyoro on 17/01/2020.
+//  Copyright © 2020 Tobi Kuyoro. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonResults: Decodable {
+    let results: [Pokemon]
+}
+
+struct Pokemon: Decodable {
+    let name: String
+    let id: Int
+    let abilities: [PokemonAbility]
+    let types: [PokemonType]
+    var sprites: PokemonSprites
+}
+
+struct PokemonAbility: Decodable {
+    let ability: NamedAbility
+}
+
+struct NamedAbility: Decodable {
+    let name: String
+}
+
+struct PokemonType: Decodable {
+    let name: String
+}
+
+struct PokemonSprites: Decodable {
+    var frontDefault: URL
+    
+    enum CodiKeys: String, CodingKey {
+        case frontDefault = "front_default"
+    }
+}
