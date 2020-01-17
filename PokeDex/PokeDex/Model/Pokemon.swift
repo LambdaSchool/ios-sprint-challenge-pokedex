d0b4bfb4feb7e6ea9cc191fb56b0b965a5206118
Aaron Cleveland <toscleveland@gmail.com>
2020-01-17 11:04:38 -0600

    Updated UI, cleaned structure and started with Model

--- PokeDex/PokeDex/Model/Pokemon.swift
+++ PokeDex/PokeDex/Model/Pokemon.swift
@@ -5,3 +5,14 @@
 //  Created by Aaron Cleveland on 1/17/20.
 //  Copyright © 2020 Aaron Cleveland. All rights reserved.
 //
+
+import Foundation
+import UIKit
+
+struct Pokemon: Codable {
+    let name: String
+    let types: String
+    let abilities: String
+    let id: Int
+    let images: UIImage
+}
