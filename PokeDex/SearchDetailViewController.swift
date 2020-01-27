0e7696e1a6b4ed33e0206305dccfe810d10c0baa
Nicolas Rios <nickrios33@yahoo.com>
2020-01-26 20:09:08 -0800

    done with challenge

---
+++ PokeDex/SearchDetailViewController.swift
@@ -0,0 +1,121 @@
+//
+//  SearchDetailViewController.swift
+//  PokeDex
+//
+//  Created by Nicolas Rios on 11/26/19.
+//  Copyright © 2019 Nicolas Rios. All rights reserved.
+//
+
+import UIKit
+
+class SearchDetailViewController: UIViewController {
+  
+    
+    @IBOutlet weak var searchBarOutlet: UISearchBar!
+    
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var idLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    @IBOutlet weak var abilityLabel: UILabel!
+    
+    @IBOutlet weak var saveButton: UIButton!
+    
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    var pokeapiController: PokeAPIController? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    
+    
+  
+override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateViews()
+        searchBarOutlet.delegate = self
+    }
+    
+    func hideKeyboard() {
+        searchBarOutlet.resignFirstResponder()
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+        guard let pokemon = pokemon else {return}
+        pokeapiController?.addPokemon(pokemon: pokemon)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func deleteButtonTapped(_ sender: UIButton) {
+        guard let pokemon = pokemon else {return}
+        pokeapiController?.delete(pokemon: pokemon)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    func updateViews() {
+        guard isViewLoaded else {return}
+        guard let pokemonObject = pokemon else {
+            title = "Pokemon Search"
+            print("Labels not updated")
+            return
+        }
+        
+        print("\(pokemonObject.name) set in detail view")
+        title = pokemonObject.name.capitalized
+        pokemonNameLabel.text = pokemon?.name
+        idLabel.text = "ID: \(pokemonObject.id)"
+        
+        var types: [String] = []
+        for typeInfo in pokemonObject.types {
+            types.append(typeInfo.type.name)
+        }
+        
+        typeLabel.text = "\(types.joined(separator: ", "))"
+        abilityLabel.text = "\(pokemonObject.abilities[0].ability.name)"
+        
+        pokeapiController?.fetchImage(from: pokemonObject.sprites.imageUrl, completion: { (pokemonImage) in
+            DispatchQueue.main.async {
+                self.imageView.image = pokemonImage
+            }
+        })
+    }
+}
+
+
+extension SearchDetailViewController: UISearchBarDelegate {
+     func searchBarSearchButtonClicked(_ sender: UISearchBar) {
+        guard let searchTerm = searchBarOutlet.text else { return }
+
+         pokeapiController?.fetchPokemon(pokemonName: searchTerm) { (pokemonObject) in
+
+             guard let pokemon = try? pokemonObject.get() else { return }
+
+
+              DispatchQueue.main.async {
+                 self.pokemon = pokemon
+             }
+         }
+        
+        guard let pokemonImgUrl = pokemon?.sprites.imageUrl else {return}
+        pokeapiController?.fetchImage(from: pokemonImgUrl, completion: { (pokemonImage) in
+            DispatchQueue.main.async {
+                self.imageView.image = pokemonImage
+            }
+        })
+        
+    }
+}
