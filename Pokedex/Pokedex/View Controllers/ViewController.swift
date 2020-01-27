3c222d0db2cf51462faf47f43e9a16aa43c91df6
Joshua Rutkowski <jr.rtkski@gmail.com>
2020-01-26 21:12:08 -0500

    Initial commit

---
+++ Pokedex/Pokedex/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Joshua Rutkowski on 1/26/20.
+//  Copyright © 2020 Josh Rutkowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
