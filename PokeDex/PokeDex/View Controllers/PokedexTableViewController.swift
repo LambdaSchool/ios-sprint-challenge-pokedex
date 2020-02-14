b7231734ff5f73c7beddcb8211383d6e3322c134
Enrique Gongora <EGTech24@gmail.com>
2020-02-14 10:27:23 -0800

    Saving progress.

--- PokeDex/PokeDex/View Controllers/PokedexTableViewController.swift
+++ PokeDex/PokeDex/View Controllers/PokedexTableViewController.swift
@@ -9,18 +9,33 @@
 import UIKit
 
 class PokedexTableViewController: UITableViewController {
+    
+    //MARK: - Variables
+    let pokemonController = PokemonController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return pokemonController.pokemonList.count
     }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        let pokemon = pokemonController.pokemonList[indexPath.row]
+        cell.textLabel?.text = pokemon.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            pokemonController.pokemonList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    //MARK: - Segue Navigation
+    // PokemonSearchSegue PokemonDetailSegue
 }
--- PokeDex/PokeDex/View Controllers/PokemonDetailViewController.swift
+++ PokeDex/PokeDex/View Controllers/PokemonDetailViewController.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
 
+    //MARK: - IBOutlet
+    @IBOutlet weak var pokemonNameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    
+    //MARK: - Variables
+    var pokemonController: PokemonController?
+    var pokemon: Pokemon?
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func updateViews() {
     }
 }
