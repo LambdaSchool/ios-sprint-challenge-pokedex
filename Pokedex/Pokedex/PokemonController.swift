f1ca607d60dcfa0b8c99920b42cb7d6f0a0621c2
Tobi Tselios <tobi.tselios@gmail.com>
2020-02-14 21:31:20 +0000

    PokemonDetailView

--- Pokedex/Pokedex/PokemonController.swift
+++ Pokedex/Pokedex/PokemonController.swift
@@ -55,12 +55,12 @@ class PokemonController {
         }.resume()
     }
     
-    func savePokemon(called pokemon: Pokemon) {
+    func save(pokemon: Pokemon) {
         let pokemon = Pokemon(name: pokemon.name, id: pokemon.id, abilities: pokemon.abilities, types: pokemon.types, sprites: pokemon.sprites)
         pokemonList.append(pokemon)
     }
     
-    func pokemon(called pokemon: Pokemon) {
+    func delete(called pokemon: Pokemon) {
         guard let pokemonIndex = pokemonList.firstIndex(of: pokemon) else { return }
         pokemonList.remove(at: pokemonIndex)
     }
--- Pokedex/Pokedex/PokemonDetailViewController.swift
+++ Pokedex/Pokedex/PokemonDetailViewController.swift
@@ -16,20 +16,88 @@ class PokemonDetailViewController: UIViewController {
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var typesLabel: UILabel!
     @IBOutlet weak var abilitiesLabel: UILabel!
+    @IBOutlet weak var savePokemonButton: UIButton!
     
+    var pokemonController: PokemonController?
     var pokemon: Pokemon?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
+        searchBar.delegate = self
     }
     
-    @IBAction func savePokemon(_ sender: Any) {
-        
+    @IBAction func savePokemonTapped(_ sender: Any) {
+        if let pokemonController = pokemonController,
+            let pokemon = pokemon {
+            pokemonController.save(pokemon: pokemon)
+            navigationController?.popViewController(animated: true)
+        }
     }
     
     func updateViews() {
-        guard let pokemon 
+        guard let pokemon = pokemon else {
+            title = "Pokemon Search"
+            hideOutlets()
+            return
+        }
+        
+        showOutlets()
+        searchBar.isHidden = true
+        pokemonName.text = pokemon.name
+        guard let imageData = try? Data(contentsOf: pokemon.sprites.picture) else { fatalError() }
+        pokemonImage.image = UIImage(data: imageData)
+        idLabel.text = "ID: \(pokemon.id)"
+        
+        var typesText: [String] = []
+        var abilitiesText: [String] = []
+        
+        for pokemonType in pokemon.types {
+            typesText.append(pokemonType.type.name)
+        }
+        
+        for pokemonAbility in pokemon.abilities {
+            abilitiesText.append(pokemonAbility.ability.name)
+        }
+        
+        typesLabel.text = "Types: \(typesText.joined(separator: ", ").capitalized)"
+        abilitiesLabel.text = "Abilities: \(abilitiesText.joined(separator: ",").capitalized)"
+        savePokemonButton.isHidden = false
+    }
+    
+    func hideOutlets() {
+        pokemonName.isHidden = true
+        pokemonImage.isHidden = true
+        pokemonImage.isHidden = true
+        idLabel.isHidden = true
+        typesLabel.isHidden = true
+        abilitiesLabel.isHidden = true
+        savePokemonButton.isHidden = true
+    }
+    
+    func showOutlets() {
+        pokemonName.isHidden = false
+        pokemonImage.isHidden = false
+        pokemonImage.isHidden = false
+        idLabel.isHidden = false
+        typesLabel.isHidden = false
+        abilitiesLabel.isHidden = false
+        savePokemonButton.isHidden = false
+    }
+}
+
+extension PokemonDetailViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let pokemonName = searchBar.text else { return }
+        
+        pokemonController?.findPokemon(called: pokemonName, completion: { (result) in
+            let pokemon = try? result.get()
+            if let pokemon = pokemon {
+                DispatchQueue.main.async {
+                    self.pokemon = pokemon
+                    self.updateViews()
+                }
+            }
+        })
     }
 }
