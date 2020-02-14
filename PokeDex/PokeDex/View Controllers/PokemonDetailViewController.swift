5434a72f3d808bcc9e46eef2b822cfb7215e5624
Enrique Gongora <EGTech24@gmail.com>
2020-02-14 10:49:37 -0800

    View Controllers all nearly completed.

--- PokeDex/PokeDex/View Controllers/PokemonDetailViewController.swift
+++ PokeDex/PokeDex/View Controllers/PokemonDetailViewController.swift
@@ -19,13 +19,45 @@ class PokemonDetailViewController: UIViewController {
     
     //MARK: - Variables
     var pokemonController: PokemonController?
-    var pokemon: Pokemon?
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
     
     //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
     
+    //MARK: - Functions
     func updateViews() {
+        guard isViewLoaded, let pokemonDetail = pokemon else {
+            title = "Pokemon Search"
+            return
+        }
+        guard let pokemonName = pokemonNameLabel.text else { return }
+        
+        title = pokemonDetail.name.capitalized
+        pokemonNameLabel.text = pokemonName
+        idLabel.text = "ID: \(pokemonDetail.id)"
+        typeLabel.text = "Type: \(pokemonDetail.types)"
+        abilityLabel.text = "Ability: \(pokemonDetail.abilities)"
+        
+        pokemonController?.fetchImage(with: pokemonDetail.sprites.defaultImageURL, completion: { (result) in
+            do {
+                let pokemonImage = try result.get()
+                self.updateImage(with: pokemonImage)
+            } catch {
+                self.imageView.image = nil
+            }
+        })
+    }
+    
+    func updateImage(with image: UIImage) {
+        DispatchQueue.main.async {
+            self.imageView.image = image
+        }
     }
 }
