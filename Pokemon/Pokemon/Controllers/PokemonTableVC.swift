b5792325493db00f89de37022f80dcccef2c33ae
Nick Nguyen <ptnguyen1901@gmail.com>
2020-02-14 17:00:16 -0500

    Implement swipe to delete

--- Pokemon/Pokemon/Controllers/PokemonTableVC.swift
+++ Pokemon/Pokemon/Controllers/PokemonTableVC.swift
@@ -37,6 +37,13 @@ class PokemonTableVC: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            pokemons.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
     
     
     // MARK: - Segue
