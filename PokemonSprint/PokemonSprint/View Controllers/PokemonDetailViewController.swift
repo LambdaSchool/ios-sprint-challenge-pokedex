7759915390245ba567a57bb82688cde3943d7623
Elizabeth <wingateep@icloud.com>
2020-02-14 13:50:37 -0500

    forgot to ad UIView in searchVC had to change around things -

--- PokemonSprint/PokemonSprint/View Controllers/PokemonDetailViewController.swift
+++ PokemonSprint/PokemonSprint/View Controllers/PokemonDetailViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class PokemonDetailViewController: UIViewController {
 
+    var pokemonController: PokemonController?
+    var pokemon: Pokemon? {
+        didSet {
+        updateViews()
+        }
+    }
+    
     // MARK: - IBOutlets
-
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var typesLabel: UILabel!
@@ -20,19 +26,48 @@ class PokemonDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateViews() {
+        if let pokemon = pokemon {
+            updatePokemonDetailViews(with: pokemon)
+        } else {
+            clearPokemonDetailViews()
+        }
     }
-    */
+      private func updatePokemonDetailViews(with pokemon: Pokemon) {
+         pokemonController?.fetchImage(at: pokemon.spriteURL) { result in
+             do {
+                 let pokemonImage = try result.get()
+                 self.updateImage(with: pokemonImage)
+             } catch {
+                 self.spriteImageView.image = nil
+             }
+         }
+
+         DispatchQueue.main.async {
+             self.nameLabel.text = pokemon.name.capitalized
+             self.idLabel.text = "ID: " + String(pokemon.id)
+             self.typesLabel.text = "Types: " + pokemon.typeNames.joined(separator: ", ").capitalized
+             self.abilitiesLabel.text = "Abilities: " + pokemon.abilityNames.joined(separator: ", ").capitalized
+         }
+     }
+
+     private func updateImage(with image: UIImage) {
+         DispatchQueue.main.async {
+             self.spriteImageView.image = image
+         }
+     }
+
+     private func clearPokemonDetailViews() {
+         DispatchQueue.main.async {
+             self.nameLabel.text = ""
+             self.idLabel.text = ""
+             self.typesLabel.text = ""
+             self.abilitiesLabel.text = ""
+             self.spriteImageView.image = nil
+         }
+     }
 
 }
