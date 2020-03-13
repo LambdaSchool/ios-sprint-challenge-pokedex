a6660f5894ec6c29e6e81ea3e023fb3ad147f001
Hector L <hectoremil21@hotmail.com>
2020-03-13 11:55:37 -0400

    Create model files

---
+++ Pokedex/Pokedex/API/PokedexController.swift
@@ -0,0 +1,13 @@
+//
+//  PokedexController.swift
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 3/13/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+class Pokedex {
+    
+}
---
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Karen Rodriguez on 3/13/20.
+//  Copyright © 2020 Hector Ledesma. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    
+}
