e4ab33aaa0877ab8971f1cf59d92f1a25272fb85
Mark Gerrior <github@gerrior.com>
2020-03-13 09:14:28 -0700

    Wired up search elements

--- Pokedex/Pokedex/View Controllers/SearchViewController.swift
+++ Pokedex/Pokedex/View Controllers/SearchViewController.swift
@@ -13,25 +13,44 @@ class SearchViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var searchBar: UISearchBar!
 
+    @IBOutlet weak var pokemonStackView: UIStackView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var abilityLabel: UILabel!
+    @IBOutlet weak var saveButtonLabel: UIButton!
     
     // MARK: - Actions
+    @IBAction func saveButton(_ sender: Any) {
+    }
     
+    // MARK: - Properites
+    var pokemon: Pokemon? {
+        didSet {
+            updateViews()
+        }
+    }
     
+    // MARK: - Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if pokemon == nil {
+            // Hide pokemon stack view
+        } else {
+            // Show pokemon stack view
+            updateViews()
+        }
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    /// Load the Pokemon object into the view
+    func updateViews() {
+        
     }
-    */
-
 }
