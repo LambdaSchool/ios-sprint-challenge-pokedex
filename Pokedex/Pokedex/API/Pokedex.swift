b488d7ef142e395b538914ab0bc69b221d477898
Hector L <hectoremil21@hotmail.com>
2020-03-13 12:36:02 -0400

    Create getpokemon method in Pokedex. Create url request.

--- Pokedex/Pokedex/API/Pokedex.swift
+++ Pokedex/Pokedex/API/Pokedex.swift
@@ -8,11 +8,34 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    
+}
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
 class Pokedex {
     var pokemon: [Pokemon] = []
-    let baseURL = URL(string: "https://pokeapi.co/api/v2")
+    let baseURL = URL(string: "https://pokeapi.co/api/v2")!
     
     // MARK: - Get pokemon
+    func getPokemon(for pokemonName: String, completion: @escaping (Result<Pokemon, NetworkError>) -> ()) {
+        
+        // MARK: - Build request URL
+        let pokemonURL = baseURL.appendingPathComponent("pokemon/\(pokemonName)")
+        var request = URLRequest(url: pokemonURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        // MARK: - Out into the world
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            <#code#>
+        }.resume()
+        
+    }
+    
     // MARK: - Save Pokemon
     
 }
