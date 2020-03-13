21d9a6769a21986460646c998985d2a805a2dd1b
Wyatt Harrell <wyattaharrell@gmail.com>
2020-03-13 11:08:35 -0400

    initial project setup

---
+++ Pokedex/Pokedex/Model Controllers/PokemonController.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonController.swift
+//  Pokedex
+//
+//  Created by Wyatt Harrell on 3/13/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+
+class PokemonController {
+    
+}
---
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Wyatt Harrell on 3/13/20.
+//  Copyright © 2020 Wyatt Harrell. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    
+}
