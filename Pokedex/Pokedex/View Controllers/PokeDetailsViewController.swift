e1bd0daf1ba46e076f84d867f63f9452a6636950
Christopher DeVito <toolofjesus@protonmail.com>
2020-03-13 11:29:07 -0400

    Initial commit - layout set up.

---
+++ Pokedex/Pokedex/View Controllers/PokeDetailsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PokeDetailsViewController.swift
+//  Pokedex
+//
+//  Created by Lambda_School_Loaner_259 on 3/13/20.
+//  Copyright © 2020 DeVitoC. All rights reserved.
+//
+
+import UIKit
+
+class PokeDetailsViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - IBOutlets
+    @IBOutlet var pokeImageView: UIImageView!
+    @IBOutlet var pokeIDLabel: UILabel!
+    @IBOutlet var pokeTypesLabel: UILabel!
+    @IBOutlet var pokeAbilitiesLabel: UILabel!
+    @IBOutlet var saveButton: UIButton!
+    
+    
+    // MARK: - IBActions
+    @IBAction func saveButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
