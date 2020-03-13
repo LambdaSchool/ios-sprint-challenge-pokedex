d5406515d48e2701a064f679dacad16bddfceacf
Shawn Gee <shawngee@gmail.com>
2020-03-13 13:33:34 -0400

    Allow deletion of pokemon

--- Pokedex/Pokedex/View Controllers/PokedexTableViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokedexTableViewController.swift
@@ -43,19 +43,14 @@ class PokedexTableViewController: UITableViewController {
         return cell
     }
 
-
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            pokedex.pokemon.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        } 
     }
-    */
-
 
 
     // MARK: - Navigation
