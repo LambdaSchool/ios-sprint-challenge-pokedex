efbefc01dcf7f0f6bb5f99812b9107e5e18afefb
Chad Parker <chad@chadparker.com>
2020-03-20 19:48:42 -0700

    Add VCs, rough storyboard setup, PokemonVC views

---
+++ Pokedex/Pokedex/View Controllers/PokemonViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonViewController.swift
+//  Pokedex
+//
+//  Created by Chad Parker on 3/20/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class PokemonViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Pokedex/Pokedex/View Controllers/SearchViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  Pokedex
+//
+//  Created by Chad Parker on 3/20/20.
+//  Copyright © 2020 Chad Parker. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }    
+
+    /*
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
