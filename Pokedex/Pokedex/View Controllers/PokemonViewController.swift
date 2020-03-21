3559ad5da822c8456af5e5aa5e1eb8e77062d198
Chad Parker <chad@chadparker.com>
2020-03-20 20:23:38 -0700

    PokemonVC setup, set outlets

--- Pokedex/Pokedex/View Controllers/PokemonViewController.swift
+++ Pokedex/Pokedex/View Controllers/PokemonViewController.swift
@@ -9,8 +9,31 @@
 import UIKit
 
 class PokemonViewController: UIViewController {
+    
+    var pokemon: Pokemon!
+    var apiController: APIController!
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateViews()
+        getImage()
+    }
+    
+    private func updateViews() {
+        nameLabel.text = pokemon.name
+        idLabel.text = "\(pokemon.id)"
+        typesLabel.text = String(describing: pokemon.types)
+        abilitiesLabel.text = String(describing: pokemon.abilities)
+    }
+    
+    private func getImage() {
+        
     }
 }
