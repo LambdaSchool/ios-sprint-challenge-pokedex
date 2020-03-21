7eddc17c80a9c89159bd1da25b3f9819033cf7c6
hai666l <0xconr@protonmail.com>
2020-03-20 20:23:27 -0500

    Pokemon Model

---
+++ Podedex/Podedex/Models/Pokemon.swift
@@ -0,0 +1,16 @@
+//
+//  Pokemon.swift
+//  Podedex
+//
+//  Created by conner on 3/20/20.
+//  Copyright © 2020 conner. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon {
+    var name: String
+    var id: String
+    var types: [String]
+    var abilities: [String]
+}
