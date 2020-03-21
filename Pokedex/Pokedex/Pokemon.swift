a19c331ac82f2f421242725143cddd964a6efb2a
morgan smith <mssmitty07@gmail.com>
2020-03-20 21:14:45 -0400

    initial commit

---
+++ Pokedex/Pokedex/Pokemon.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Morgan Smith on 1/27/20.
+//  Copyright © 2020 Morgan Smith. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    var id: Int
+    var name: String
+    var ability: String
+    var type: String
+    
+}
