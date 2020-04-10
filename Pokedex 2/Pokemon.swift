eedf60fcedc84a2110e0676d10773ef810e9ccf2
Bharat Kumar <Bharatiosdeveloper@gmail.com>
2020-04-10 09:01:05 -0700

    Started with the project

---
+++ Pokedex 2/Pokemon.swift
@@ -0,0 +1,13 @@
+//
+//  Pokemon.swift
+//  Pokedex 2
+//
+//  Created by Bhawnish Kumar on 3/14/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    
+}
