fc7047a6e152883f8b6aeba8537b87254c0de5f4
Harmony Radley <harmonyradley99@gmail.com>
2020-04-10 12:38:42 -0400

    Finished Model and Model Controller

--- Pokedex/Pokedex/Controller/PokemonController.swift
+++ Pokedex/Pokedex/Controller/PokemonController.swift
@@ -7,14 +7,92 @@
 //
 
 import Foundation
+import UIKit
 
-struct PokemonController {
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case noDecode
+}
+
+class PokemonController {
     
+    var pokemon: [Pokemon] = []
     
+    private let baseURL = URL(string: "https://pokeapi.co/api/v2")!
+  
+
+    func fetchAllPokemon(with name: String, completion: @escaping (Result<Pokemon, NetworkError?>) -> Void) {
+        let allPokemonURL = baseURL.appendingPathComponent("pokemon")
+        
+        var request = URLRequest(url: allPokemonURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+                   if let response = response as? HTTPURLResponse,
+                       response.statusCode == 401 {
+                       completion(.badAuth)
+                       return
+                   }
+                   
+                   if let error = error {
+                    completion(.failure(.otherError))
+                       return
+                   }
+                   
+                   guard let data = data else {
+                       completion(.badData)
+                       return
+                   }
+
+                   let decoder = JSONDecoder()
+                   decoder.dateDecodingStrategy = .iso8601
+                   do {
+                       self.pokemon = try decoder.decode([Pokemon].self, from: data)
+                       completion(nil)
+                   } catch {
+                       print("Error decoding Pokemon objects: \(error)")
+                       completion(.noDecode)
+                       return
+                   }
+                   }.resume()
+    }
     
-    
-    
-    
+    func fetchImage(at urlString: String, completion: @escaping (Result<UIImage, NetworkError>) -> Void) {
+        guard let imageUrl = URL(string: urlString) else {
+            completion(.failure(.otherError))
+            return
+        }
+
+        var request = URLRequest(url: imageUrl)
+        request.httpMethod = HTTPMethod.get.rawValue
+
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            guard error == nil else {
+                completion(.failure(.otherError))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(.noAuth))
+                return
+            }
+
+            guard let image = UIImage(data: data) else {
+                completion(.failure(.noDecode))
+                return
+            }
+
+            completion(.success(image))
+        }.resume()
+    }
     
     
     
--- Pokedex/Pokedex/Model/Pokemon.swift
+++ Pokedex/Pokedex/Model/Pokemon.swift
@@ -9,41 +9,8 @@
 import Foundation
 
 struct Pokemon: Codable {
+    let id: Int
     let name: String
     let types: String
     let abilities: [String]
-    
-    enum PokemonKeys: String, CodingKey {
-            case name
-            case abilities
-            
-            enum AbilityContentKeys: String, CodingKey {
-                case ability
-                
-                enum AbilityNameKeys: String, CodingKey {
-                    case name
-                }
-            }
-        }
-        
-        init(from decoder: Decoder) throws {
-            let container = try decoder.container(keyedBy: PokemonKeys.self)
-            name = try container.decode(String.self, forKey:  .name)
-            
-            var abilitiesContainer = try container.nestedUnkeyedContainer(forKey: .abilities)
-            var abilityNames: [String] = []
-            while !abilitiesContainer.isAtEnd {
-                let abilityContentContainer = try abilitiesContainer.nestedContainer(keyedBy: PokemonKeys.AbilityContentKeys.self)
-                let abilityNameContainer = try abilityContentContainer.nestedContainer(keyedBy: PokemonKeys.AbilityContentKeys.AbilityNameKeys.self, forKey: .ability)
-                let abilityName = try abilityNameContainer.decode(String.self, forKey: .name)
-                abilityNames.append(abilityName)
-            }
-            abilities = abilityNames
-        }
-    }
-
-    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon/ditto")!
-    let data = try! Data(contentsOf: baseURL)
-    let pokemon = try! JSONDecoder().decode(Pokemon.self, from: data)
-
-
+}
