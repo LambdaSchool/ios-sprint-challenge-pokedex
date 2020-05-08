5be5140e4ce943d3ae63850fbbe12d017541a480
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-05-08 10:03:26 -0700

    Adding to storyboard

--- Pokedex/Pokedex/Model Controller/Models/Pokemon.swift
+++ Pokedex/Pokedex/Model Controller/Models/Pokemon.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+struct Pokemon: Codable, Equatable {
+    let name: String
+    let types: [TypeInfo]
+    let abilities: [AbilityInfo]
+    let id: Int
+    let sprites: SpriteFront
+
+struct AbilityInfo: Codable, Equatable {
+     let ability: Ability
+
+     struct Ability: Codable, Equatable {
+         let name: String
+     }
+ }
+
+ struct TypeInfo: Codable, Equatable {
+     let type:TypeOfType
+
+    struct TypeOfType: Codable, Equatable {
+         let name: String
+     }
+ }
+
+
+ struct SpriteFront: Codable, Equatable {
+     let imageUrl: String
+
+     enum CodingKeys: String, CodingKey {
+         case imageUrl = "front_default"
+     }
+ }
+}
--- Pokedex/Pokedex/Model Controller/PokemonController.swift
+++ Pokedex/Pokedex/Model Controller/PokemonController.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case delete = "DELETE"
+}
+enum NetworkError: Error {
+    case noData, noDecode, badURL, incompleteData, tryAgain
+}
+
+class PokemonController {
+    var pokemonArray: [Pokemon] = []
+    var pokemonImages: [URL] = []
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+    
+    // MARK: - FETCH IMAGE FUNCTION
+    
+    func fetchImage(at urlString: String, completion: @escaping (Result<UIImage, NetworkError>) -> Void) {
+        let imageURL = URL(string: urlString)!
+        
+        var request = URLRequest(url: imageURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+        if let error = error {
+            NSLog("Error fetching pokemon image: \(error)")
+            completion(.failure(.noData))
+            return
+        }
+        guard let data = data else {
+            NSLog("No data provided for image: \(error)")
+            completion(.failure(.noData))
+            return
+            }
+        guard let image = UIImage(data: data) else {
+            NSLog("Data for image is broken")
+            completion(.failure(.incompleteData))
+            return
+                
+        }
+            completion(.sucess(.image))
+            .resume
+        }
+        
+        
+        // MARK: ADD POKEMON
+    func addPokemon(pokemon: Pokemon) {
+        pokemonArray.append(pokemon)
+        }
+        // MARK: DELETE POKEMON
+    func delete(pokemon: Pokemon) {
+        guard let index = pokemonArray.firstIndex(of: pokemon) else { return }
+        pokemonArray.remove(at: index)
+        }
+    
+    
+ 
+    // MARK: - TODO: ADD PERSISTENCE
+
+}
+}
