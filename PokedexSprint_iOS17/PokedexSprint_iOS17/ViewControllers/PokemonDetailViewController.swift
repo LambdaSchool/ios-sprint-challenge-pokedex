fa577e393c75a11da2a39a6f52b02122c9a5fdd6
Stephanie Ballard <user.name>
2020-05-08 10:23:35 -0700

    files created and classes assigned

---
+++ PokedexSprint_iOS17/PokedexSprint_iOS17/ViewControllers/PokemonDetailViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PokemonDetailViewController.swift
+//  PokedexSprint_iOS17
+//
+//  Created by Stephanie Ballard on 5/8/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        searchbar.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
