9d77a941ee3744475464f8a2658b4afd8edb0e62
Cody Morley <codymorleyinc@gmail.com>
2020-05-08 09:18:37 -0700

    ..

---
+++ Pokemon/Pokemon/View Controllers/PokemonDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokemonDetailViewController.swift
+//  Pokemon
+//
+//  Created by Cody Morley on 5/8/20.
+//  Copyright © 2020 Cody Morley. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
