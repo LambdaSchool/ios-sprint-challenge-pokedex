f9058fc9255f2cfd9e76644cbc9575f591522b0c
Ronald Huston Jr <rghuston9@gmail.com>
2020-05-08 18:49:50 -0500

    PokedexTableVC

--- pokedex/pokedex/view controllers/PokedexTableViewController.swift
+++ pokedex/pokedex/view controllers/PokedexTableViewController.swift
@@ -12,47 +12,54 @@ class PokedexTableViewController: UITableViewController {
 
     let pokemonController = PokemonController()
     
-    
-    
-    
+    var pokemon: Pokemon! {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.reloadData()
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
 
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return pokemonController.pokemonArray.count
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PokemonCell", for: indexPath)
+        let pokemonFigure = pokemonController.pokemonArray[indexPath.row]
+        
+        cell.textLabel?.text = pokemonFigure.name
         return cell
     }
-    */
 
    
-
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "DetailsSegue" {
+            guard let detailVC = segue.destination as? SearchDetailViewController,
+                let indexPath = tableView.indexPathForSelectedRow else { return }
+            
+            detailVC.pokemonController = pokemonController
+            detailVC.pokemon = pokemonController.pokemonArray[indexPath.row]
+        } else if segue.identifier == "SearchSegue" {
+            if let searchVC = segue.destination as? SearchDetailViewController {
+                searchVC.pokemonController = pokemonController
+            }
+        }
     }
 
 }
