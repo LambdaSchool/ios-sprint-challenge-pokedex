22fd2e67c2eef5a5b29dbc51b6a0962f52844c7b
Cody Morley <codymorleyinc@gmail.com>
2020-05-08 09:47:05 -0700

    Done testing and fixed minor bug in lower stack view display so it will always display the fields text rather than simply replacing it. MVP.

--- Pokemon/Pokemon/View Controllers/PokemonDetailViewController.swift
+++ Pokemon/Pokemon/View Controllers/PokemonDetailViewController.swift
@@ -7,24 +7,56 @@
 //
 
 import UIKit
+import Foundation
 
 class PokemonDetailViewController: UIViewController {
-
+    //MARK: - Properties -
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    var pokemon: Pokemon?
+    
+    
+    //MARK: - Life Cycles -
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Methods -
+    func updateViews() {
+        guard let pokemon = pokemon else { return }
+        print(pokemon)
+        
+        nameLabel.text = pokemon.name
+        idLabel.text = String("ID#: \(pokemon.id)")
+        
+        let imageURLString = pokemon.sprites.front_default
+        guard let imageURL = URL(string: imageURLString) else { return }
+        do {
+            let imageData = try Data(contentsOf: imageURL)
+            let image = UIImage(data: imageData)
+            imageView.image = image
+        } catch {
+            print("error loading image \(error) \(error.localizedDescription)")
+        }
+        
+        var typeNames: [String] = []
+        for type in pokemon.types {
+            typeNames.append(type.type.name)
+        }
+        let typesString = typeNames.joined(separator: ", ")
+        typesLabel.text = "Types: \(typesString)"
+        
+        var abilityNames: [String] = []
+        for ability in pokemon.abilities {
+            abilityNames.append(ability.ability.name)
+        }
+        let abilitiesString = abilityNames.joined(separator: ", ")
+        abilitiesLabel.text = "Abilities: \(abilitiesString)"
     }
-    */
-
 }
--- Pokemon/Pokemon/View Controllers/PokemonSearchViewController.swift
+++ Pokemon/Pokemon/View Controllers/PokemonSearchViewController.swift
@@ -7,24 +7,86 @@
 //
 
 import UIKit
+import Foundation
 
 class PokemonSearchViewController: UIViewController {
-
+    //MARK: - Properties -
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var typesLabel: UILabel!
+    @IBOutlet weak var abilitiesLabel: UILabel!
+    
+    var pokemonController: PokemonController?
+    var pokemon: Pokemon?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
         // Do any additional setup after loading the view.
+        print("hello world")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    //MARK: - Actions -
+    @IBAction func savePokemon(_ sender: Any) {
+        guard let unwrappedPokemon = pokemon else { return }
+        pokemonController?.savePokemon(pokemon: unwrappedPokemon)
+        
+        navigationController?.popToRootViewController(animated: true)
     }
-    */
-
+    
+    //MARK: - Methods -
+    func updateViews() {
+        guard let pokemon = pokemon else { return }
+        
+        nameLabel.text = pokemon.name
+        idLabel.text = String("ID#: \(pokemon.id)")
+        
+        
+        let imageURLString = pokemon.sprites.front_default
+        guard let imageURL = URL(string: imageURLString) else { return }
+        do {
+            let imageData = try Data(contentsOf: imageURL)
+            let image = UIImage(data: imageData)
+            imageView.image = image
+        } catch {
+            print("error loading image \(error) \(error.localizedDescription)")
+        }
+        
+        var typeNames: [String] = []
+        for type in pokemon.types {
+            typeNames.append(type.type.name)
+        }
+        let typesString = typeNames.joined(separator: ", ")
+        typesLabel.text = "Types: \(typesString)"
+        
+        var abilityNames: [String] = []
+        for ability in pokemon.abilities {
+            abilityNames.append(ability.ability.name)
+        }
+        let abilitiesString = abilityNames.joined(separator: ", ")
+        abilitiesLabel.text = "Abilities: \(abilitiesString)"
+    }
 }
+
+extension PokemonSearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let text = searchBar.text else { return }
+        pokemonController?.getPokemon(for: text, completion: { (result) in
+            switch result {
+            case .failure(let error):
+                print(error)
+            case .success(let pokemon):
+                print(pokemon)
+                self.pokemon = pokemon
+                
+                DispatchQueue.main.async {
+                    self.updateViews()
+                }
+            }
+        })
+    }
+}
+
