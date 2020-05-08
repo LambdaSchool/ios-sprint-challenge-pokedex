50b75ebbf0e774472b5f5e7aaec739455afb5226
Ronald Huston Jr <rghuston9@gmail.com>
2020-05-08 17:30:15 -0500

    SearchDetailVC built

--- pokedex/pokedex/view controllers/SearchDetailViewController.swift
+++ pokedex/pokedex/view controllers/SearchDetailViewController.swift
@@ -41,10 +41,76 @@ class SearchDetailViewController: UIViewController {
     }
     
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateViews()
+    }
+    
     func updateViews() {
         
+        if let pokemon = pokemon, isViewLoaded {
+            title = pokemon.name
+            pokemonLabel.text = pokemon.name
+            idLabel.text = "ID: \(pokemon.id)"
+            
+            var types: String = "\(pokemon.types[0].type.name)"
+            if pokemon.types.count > 1 {
+                for i in 1..<pokemon.types.count {
+                    types.append(", \(pokemon.types[i].type.name)")
+                }
+            }
+            
+            typeLabel.text = "Types: \(types)"
+            
+            var ableText: String = "\(pokemon.abilities[0].ability.name)"
+            
+            if pokemon.abilities.count > 1 {
+                for i in 1..<pokemon.abilities.count {
+                    ableText.append(", \(pokemon.abilities[i].ability.name)")
+                }
+            }
+            
+            abilityLabel.text = "Abilities: \(ableText)"
+            
+            guard let imageURL = URL(string: pokemon.picture.url) else { return }
+            imageView.load(url: imageURL)
+            unhideLabel()
+        } else {
+            title = "search pokemon"
+        }
     }
+    
+    func unhideLabel() {
+        if pokemon != nil, isViewLoaded {
+            pokemonLabel.isHidden = false
+            idLabel.isHidden = false
+            typeLabel.isHidden = false
+            abilityLabel.isHidden = false
+            saveButton.isHidden = false
+        } else {
+            pokemonLabel.isHidden = true
+            idLabel.isHidden = true
+            typeLabel.isHidden = true
+            abilityLabel.isHidden = true
+            saveButton.isHidden = true
+        }
+    }
+    
 
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
 }
 
 //  search bar delegate
@@ -55,7 +121,8 @@ extension SearchDetailViewController: UISearchBarDelegate {
             pokemonController?.fetchPokemon(pokemon: pokeName) { result in
                 if let pokeName = try? result.get() {
                     DispatchQueue.main.async {
-                        self.
+                        self.pokemon = pokeName
+                        self.updateViews()
                     }
                 }
             }
