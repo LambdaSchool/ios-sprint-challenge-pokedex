e4aea8fed1568eab5daff6ed2bbc2fe5b0f40171
Hannah <hannahlbain89@gmail.com>
2020-05-16 14:01:38 -0500

    Scenes added, controllers, models added, final commit.

---
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Hannah Bain on 5/16/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+    let name: String
+    
+    
+}
+
+
