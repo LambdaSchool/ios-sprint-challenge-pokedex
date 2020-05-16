dcdb28723e266cb18af71023e0b311969bc25e2f
bohdan462 <tech.bohdan@gmail.com>
2020-05-16 11:46:15 -0500

    Added UI

---
+++ Pokedex/Pokedex/Views/PokemonTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonTableViewCell.swift
+//  Pokedex
+//
+//  Created by Bohdan Tkachenko on 5/16/20.
+//  Copyright © 2020 Bohdan Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class PokemonTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
