9d1aed42b021538cd1272bca28bbae4294cc96d8
Sammy Alvarado <isammyalvarado@gmail.com>
2020-07-19 23:59:20 -0400

    Finished Pokemon Model
    Created a Pokemon Mode that will decode the JASON file.

--- Pokedex/Pokedex/Models/Pokemon.swift
+++ Pokedex/Pokedex/Models/Pokemon.swift
@@ -12,25 +12,51 @@ class Pokemon: Codable {
 
     let name: String
     let id: Int
-    let type: String
-    let images: [URL]
+    let image: [URL]
+    let types: [String]
 
     enum PokemonKeys: String, CodingKey {
-        case name, id, types
+        case name
+        case id
+        case types
         case image = "sprites"
-    }
 
-    enum SpritesKeys: String, CodingKey {
-        case frontDefult = "front_default"
-    }
+        enum SpritesKeys: String, CodingKey {
+            case frontDefault = "front_default"
+        }
 
-    enum TypeDescriptionKeys: String, CodingKey {
-        case type
+        enum TypeDescriptionKeys: String, CodingKey {
+            case type
 
-        enum TypeKey: String, CodingKey {
-            case name
+            enum TypeKey: String, CodingKey {
+                case name
+            }
         }
     }
 
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: PokemonKeys.self)
 
+        name = try container.decode(String.self, forKey: .name)
+
+        let idString = try container.decode(String.self, forKey: .id)
+        id = Int(idString) ?? 0
+
+        image = try container.decode([URL].self, forKey: .image)
+
+        var typeContainer = try container.nestedUnkeyedContainer(forKey: .types)
+        var typeNames: [String] = []
+
+        while typeContainer.isAtEnd == false {
+            let typeDescriptionContainer = try typeContainer.nestedContainer(keyedBy: PokemonKeys.TypeDescriptionKeys.self)
+
+            let typeContaienrs = try typeDescriptionContainer.nestedContainer(keyedBy: PokemonKeys.TypeDescriptionKeys.TypeKey.self, forKey: .type)
+
+            let typeName = try typeContaienrs.decode(String.self, forKey: .name)
+            typeNames.append(typeName)
+        }
+        types = typeNames
+
+
+    }
 }
